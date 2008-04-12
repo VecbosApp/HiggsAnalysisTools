34d21ef81d5b9d41b23cddf0e67439d8121c51fd
emanuele <>
2008-04-12 16:23:54 +0000

    add the cuts on pthigh and ptlow lepton (for electron case now)

--- include/CutBasedHiggsSelector.hh
+++ include/CutBasedHiggsSelector.hh
@@ -18,20 +18,20 @@ public:
   void Configure(const char *configDir);
 
   //! set event by event observables
-  void SetWeight(float weight)             { m_weight        = weight;    }
-  void SetHighElePt(float highPt)          { m_highPt        = highPt;    }
-  void SetLowElePt(float lowPt)            { m_lowPt         = lowPt;     }
-  void SetInvMass(float mll)               { m_invMass       = mll;       }
-  void SetElectronId(bool isEleId)         { m_isElectronId  = isEleId; }
-  void SetPositronId(bool isPosId)         { m_isPositronId  = isPosId; }
-  void SetEleTrackerPtSum(float eleTkPtSum){ m_eleTkPtSum    = eleTkPtSum; }
-  void SetPosTrackerPtSum(float posTkPtSum){ m_posTkPtSum    = posTkPtSum; }
-  void SetEleCaloPtSum(float eleCaloPtSum) { m_eleCaloPtSum  = eleCaloPtSum; }
-  void SetPosCaloPtSum(float posCaloPtSum) { m_posCaloPtSum  = posCaloPtSum; }
-  void SetJetVeto(float passedCJV)         { m_passedJetVeto = passedCJV; }
-  void SetMet(float met)                   { m_met           = met;}
-  void SetDeltaPhi(float deltaPhi)         { m_deltaPhi      = deltaPhi;}
-  void SetDetaLeptons(float deltaEta)      { m_detaLeptons   = deltaEta;}
+  void SetWeight(float weight)                { m_weight        = weight;    }
+  void SetHighElePt(float highPt)             { m_highPt        = highPt;    }
+  void SetLowElePt(float lowPt)               { m_lowPt         = lowPt;     }
+  void SetInvMass(float mll)                  { m_invMass       = mll;       }
+  void SetElectronId(bool isEleId)            { m_isElectronId  = isEleId; }
+  void SetPositronId(bool isPosId)            { m_isPositronId  = isPosId; }
+  void SetEleTrackerPtSum(float eleTkPtSum)   { m_eleTkPtSum    = eleTkPtSum; }
+  void SetPosTrackerPtSum(float posTkPtSum)   { m_posTkPtSum    = posTkPtSum; }
+  void SetEleCaloPtSum(float eleCaloPtSum)    { m_eleCaloPtSum  = eleCaloPtSum; }
+  void SetPosCaloPtSum(float posCaloPtSum)    { m_posCaloPtSum  = posCaloPtSum; }
+  void SetJetVeto(float passedCJV)            { m_passedJetVeto = passedCJV; }
+  void SetMet(float met)                      { m_met           = met;}
+  void SetDeltaPhi(float deltaPhi)            { m_deltaPhi      = deltaPhi;}
+  void SetDetaLeptons(float deltaEta)         { m_detaLeptons   = deltaEta;}
 
   //! get output of the selector
   bool output();
@@ -49,7 +49,8 @@ private:
   float m_posTkPtSum, m_posCaloPtSum;
   bool m_passedJetVeto;
   float m_met, m_deltaPhi, m_detaLeptons;
-  
+  float m_maxPtElectron, m_minPtElectron;
+
   //! contains the preselection cuts
   Selection* _selection;
 
