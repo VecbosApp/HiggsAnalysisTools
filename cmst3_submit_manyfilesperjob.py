e23150cd2448c5e6308bb560a775ad3647215e25
emanuele <>
2009-04-02 17:18:15 +0000

    some updates to scp in a local disk

--- cmst3_submit_manyfilesperjob.py
+++ cmst3_submit_manyfilesperjob.py
@@ -3,37 +3,33 @@ import os
 import sys
 # set parameters to use cmst3 batch 
 #######################################
+### usage  cmst3_submit_manyfilesperjob.py dataset njobs applicationName queue 
+#######################################
 if len(sys.argv) != 5:
-    print "usage python cmst3_submit_manyfilesperjob.py dataset njobs applicationName queue"
+    print "usage cmst3_submit_manyfilesperjob.py dataset njobs applicationName queue"
     sys.exit(1)
 dataset = sys.argv[1]
+inputlist = "cmst3_21X/"+dataset+".list"
+output = dataset
+queue = sys.argv[4]
 ijobmax = int(sys.argv[2])
 application = sys.argv[3]
-inputlist = "cmst3_21X/"+dataset+".list"
-queue = sys.argv[4] # choose among cmt3 8nm 1nh 8nh 1nd 1nw 
-output = dataset+"_symm"
 # to write on the cmst3 cluster disks
 ################################################
 #castordir = "/castor/cern.ch/user/m/mpierini/CMST3/Vecbos/output/"
 #outputmain = castordir+output
 # to write on local disks
 ################################################
-castordir = "none"
-outputmain = output
+castordir = "/castor/cern.ch/user/e/emanuele/Higgs21X/Results/"
+diskoutputdir = "/cmsrm/pc21/emanuele/data/Higgs2.1.X/results1/"
+outputmain = castordir+output
+diskoutputmain = diskoutputdir+output
 # prepare job to write on the cmst3 cluster disks
 ################################################
-os.system("\\rm -f tmp.log")
-os.system("echo $PWD > tmp.log")
-tmp = open("tmp.log")
-pwd = tmp.readline()
-tmp.close()
-os.system("\\rm -f tmp.log")
-#######################################
 os.system("mkdir "+output)
 os.system("mkdir "+output+"/log/")
 os.system("mkdir "+output+"/input/")
 os.system("mkdir "+output+"/src/")
-os.system("cp -r "+pwd[:-1]+'/'+'config '+output)
 outputroot = outputmain+"/root/"
 if castordir != "none": 
     os.system("rfmkdir "+outputmain)
@@ -41,7 +37,13 @@ if castordir != "none":
     os.system("rfchmod 777 "+outputmain)
     os.system("rfchmod 777 "+outputroot)
 else: os.system("mkdir "+outputroot)
+
+if diskoutputdir != "none": 
+    os.system("ssh -o BatchMode=yes -o StrictHostKeyChecking=no pccmsrm21 mkdir "+diskoutputmain)
+
 #look for the current directory
+#######################################
+pwd = os.environ['PWD']
 #######################################
 numfiles = reduce(lambda x,y: x+1, file(inputlist).xreadlines(), 0)
 filesperjob = numfiles/ijobmax
@@ -51,7 +53,7 @@ input = open(inputlist)
 
 for ijob in range(ijobmax):
     # prepare the list file
-    inputfilename = output+"/input/input_"+str(ijob)+".list"
+    inputfilename = pwd+"/"+output+"/input/input_"+str(ijob)+".list"
     inputfile = open(inputfilename,'w')
     # if it is a normal job get filesperjob lines
     if ijob != (ijobmax-1):
@@ -74,12 +76,13 @@ for ijob in range(ijobmax):
     outputfile.write('#!/bin/bash\n')
     outputfile.write('export STAGE_HOST=castorcms\n')
     outputfile.write('export STAGE_SVCCLASS=cmst3\n')
-    outputfile.write('cd $WORKDIR \n')
-    outputfile.write('cp -r '+pwd[:-1]+'/'+output+'/config $WORKDIR \n')
-    outputfile.write(pwd[:-1]+'/'+application+' '+pwd[:-1]+"/"+inputfilename+" "+output+"_"+str(ijob)+"\n")
-    outputfile.write('ls *.root | xargs -i cp {} '+pwd[:-1]+"/"+outputroot+'\n')
+    outputfile.write('cp -r '+pwd+'/config $WORKDIR\n')
+    outputfile.write('cd $WORKDIR\n')
+    outputfile.write(pwd+'/'+application+' '+inputfilename+" "+output+"_"+str(ijob)+" "+"\n")
+    outputfile.write('rfcp *.root '+outputroot+'\n')
+    outputfile.write('scp -o BatchMode=yes -o StrictHostKeyChecking=no *.root pccmsrm21:'+diskoutputmain+'/{}\n') 
     outputfile.close
-    os.system("echo bsub -q "+queue+" -o "+output+"/log/"+output+"_"+str(ijob)+".log source "+pwd[:-1]+"/"+outputname)
-    os.system("bsub -q "+queue+" -o "+output+"/log/"+output+"_"+str(ijob)+".log source "+pwd[:-1]+"/"+outputname)
+    os.system("echo bsub -q "+queue+" -o "+output+"/log/"+output+"_"+str(ijob)+".log source "+pwd+"/"+outputname)
+    os.system("bsub -q "+queue+" -o "+output+"/log/"+output+"_"+str(ijob)+".log source "+pwd+"/"+outputname)
     ijob = ijob+1
     continue
