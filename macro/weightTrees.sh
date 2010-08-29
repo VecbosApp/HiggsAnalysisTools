2312676570f5ba688ff7c6fa752ebedefcafa703
emanuele <>
2010-08-29 10:21:49 +0000

    weights / pb-1

--- macro/weightTrees.sh
+++ macro/weightTrees.sh
@@ -5,26 +5,26 @@
 
 # usage: ./weightTrees.sh
 
-# PS. weights are compute for 100 pb-1
+# PS. weights are compute for 1000 pb-1
 
 echo "Adding weights for ee datasets..."
 root -l -b <<EOF
 
 .L addWeightsToTree.cc+
 
-addWeights("results/merged/H130_ee.root", 0.00452859);
-addWeights("results/merged/H160_ee.root", 0.00818843);
-addWeights("results/merged/H190_ee.root", 0.00486346);
-addWeights("results/merged/TTbar_ee.root", 0.128565);
-addWeights("results/merged/SingleTop_sChannel_ee.root", 0.00477608);
-addWeights("results/merged/SingleTop_tChannel_ee.root", 0.04265);
-addWeights("results/merged/SingleTop_tWChannel_ee.root", 0.644887);
-addWeights("results/merged/Wgamma_ee.root", 0.390098);
-addWeights("results/merged/WjetsMadgraph_ee.root", 2.97267);
-addWeights("results/merged/WW_ee.root", 0.0203685);
-addWeights("results/merged/WZ_ee.root", 0.00549947);
-addWeights("results/merged/ZjetsMadgraph_ee.root", 4.65717); 
-addWeights("results/merged/ZZ_ee.root", 0.00231513);
+addWeights("results/merged/H130_ee.root", 0.001 * 0.00452859);
+addWeights("results/merged/H160_ee.root", 0.001 * 0.00818843);
+addWeights("results/merged/H190_ee.root", 0.001 * 0.00486346);
+addWeights("results/merged/TTbar_ee.root", 0.001 * 0.128565);
+addWeights("results/merged/SingleTop_sChannel_ee.root", 0.001 * 0.00477608);
+addWeights("results/merged/SingleTop_tChannel_ee.root", 0.001 * 0.04265);
+addWeights("results/merged/SingleTop_tWChannel_ee.root", 0.001 * 0.644887);
+addWeights("results/merged/Wgamma_ee.root", 0.001 * 0.390098);
+addWeights("results/merged/WjetsMadgraph_ee.root", 0.001 * 2.97267);
+addWeights("results/merged/WW_ee.root", 0.001 * 0.0203685);
+addWeights("results/merged/WZ_ee.root", 0.001 * 0.00549947);
+addWeights("results/merged/ZjetsMadgraph_ee.root", 0.001 * 4.65717); 
+addWeights("results/merged/ZZ_ee.root", 0.001 * 0.00231513);
 
 .q
 
@@ -35,19 +35,19 @@ root -l -b <<EOF
 
 gSystem->Load("addWeightsToTree_cc.so");
 
-addWeights("results/merged/H130_mm.root", 0.00452859);
-addWeights("results/merged/H160_mm.root", 0.00818843);
-addWeights("results/merged/H190_mm.root", 0.00486346);
-addWeights("results/merged/TTbar_mm.root", 0.128565);
-addWeights("results/merged/SingleTop_sChannel_mm.root", 0.00477608);
-addWeights("results/merged/SingleTop_tChannel_mm.root", 0.04265);
-addWeights("results/merged/SingleTop_tWChannel_mm.root", 0.644887);
-addWeights("results/merged/Wgamma_mm.root", 0.390098);
-addWeights("results/merged/WjetsMadgraph_mm.root", 2.97267);
-addWeights("results/merged/WW_mm.root", 0.0203685);
-addWeights("results/merged/WZ_mm.root", 0.00549947);
-addWeights("results/merged/ZjetsMadgraph_mm.root", 4.65717); 
-addWeights("results/merged/ZZ_mm.root", 0.00231513);
+addWeights("results/merged/H130_mm.root", 0.001 * 0.00452859);
+addWeights("results/merged/H160_mm.root", 0.001 * 0.00818843);
+addWeights("results/merged/H190_mm.root", 0.001 * 0.00486346);
+addWeights("results/merged/TTbar_mm.root", 0.001 * 0.128565);
+addWeights("results/merged/SingleTop_sChannel_mm.root", 0.001 * 0.00477608);
+addWeights("results/merged/SingleTop_tChannel_mm.root", 0.001 * 0.04265);
+addWeights("results/merged/SingleTop_tWChannel_mm.root", 0.001 * 0.644887);
+addWeights("results/merged/Wgamma_mm.root", 0.001 * 0.390098);
+addWeights("results/merged/WjetsMadgraph_mm.root", 0.001 * 2.97267);
+addWeights("results/merged/WW_mm.root", 0.001 * 0.0203685);
+addWeights("results/merged/WZ_mm.root", 0.001 * 0.00549947);
+addWeights("results/merged/ZjetsMadgraph_mm.root", 0.001 * 4.65717); 
+addWeights("results/merged/ZZ_mm.root", 0.001 * 0.00231513);
 
 .q
 
@@ -59,19 +59,19 @@ root -l -b <<EOF
 
 gSystem->Load("addWeightsToTree_cc.so");
 
-addWeights("results/merged/H130_em.root", 0.00452859);
-addWeights("results/merged/H160_em.root", 0.00818843);
-addWeights("results/merged/H190_em.root", 0.00486346);
-addWeights("results/merged/TTbar_em.root", 0.128565);
-addWeights("results/merged/SingleTop_sChannel_em.root", 0.00477608);
-addWeights("results/merged/SingleTop_tChannel_em.root", 0.04265);
-addWeights("results/merged/SingleTop_tWChannel_em.root", 0.644887);
-addWeights("results/merged/Wgamma_em.root", 0.390098);
-addWeights("results/merged/WjetsMadgraph_em.root", 2.97267);
-addWeights("results/merged/WW_em.root", 0.0203685);
-addWeights("results/merged/WZ_em.root", 0.00549947);
-addWeights("results/merged/ZjetsMadgraph_em.root", 4.65717); 
-addWeights("results/merged/ZZ_em.root", 0.00231513);
+addWeights("results/merged/H130_em.root", 0.001 * 0.00452859);
+addWeights("results/merged/H160_em.root", 0.001 * 0.00818843);
+addWeights("results/merged/H190_em.root", 0.001 * 0.00486346);
+addWeights("results/merged/TTbar_em.root", 0.001 * 0.128565);
+addWeights("results/merged/SingleTop_sChannel_em.root", 0.001 * 0.00477608);
+addWeights("results/merged/SingleTop_tChannel_em.root", 0.001 * 0.04265);
+addWeights("results/merged/SingleTop_tWChannel_em.root", 0.001 * 0.644887);
+addWeights("results/merged/Wgamma_em.root", 0.001 * 0.390098);
+addWeights("results/merged/WjetsMadgraph_em.root", 0.001 * 2.97267);
+addWeights("results/merged/WW_em.root", 0.001 * 0.0203685);
+addWeights("results/merged/WZ_em.root", 0.001 * 0.00549947);
+addWeights("results/merged/ZjetsMadgraph_em.root", 0.001 * 4.65717); 
+addWeights("results/merged/ZZ_em.root", 0.001 * 0.00231513);
 
 .q
 
