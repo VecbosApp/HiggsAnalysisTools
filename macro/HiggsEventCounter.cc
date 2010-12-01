cd017465c87a4eca0506fab58789ba232c3cf943
emanuele <>
2010-12-01 15:42:32 +0000

    add more samples

--- macro/HiggsEventCounter.cc
+++ macro/HiggsEventCounter.cc
@@ -15,8 +15,8 @@ void countEvents() {
 
   cout << "nametree = " << nametree << endl;
 
-  TChain *chains[14];
-  for(int isample=0; isample<14; isample++) {
+  TChain *chains[17];
+  for(int isample=0; isample<17; isample++) {
     chains[isample] = new TChain(nametree);
   }
 
@@ -24,22 +24,26 @@ void countEvents() {
   chains[1]->Add("results/WPYTHIA/WToMuNu_TuneZ2/*Counters.root");
   chains[2]->Add("results/WPYTHIA/WToTauNu_TuneZ2_7TeV-pythia6-tauola/*Counters.root");
 
-  chains[3]->Add("results/ZPYTHIA/DYToEE_M-20_CT10_TuneZ2_PU/*Counters.root");
-  chains[4]->Add("results/ZPYTHIA/DYToMuMu_M-20_CT10_TuneZ2_PU/*Counters.root");
-  chains[5]->Add("results/ZPYTHIA/DYToTauTau_M-20_CT10_TuneZ2_7TeV-powheg-pythia-tauola/*Counters.root");
+  chains[3]->Add("results/ZPYTHIA/DYToEE_M-10To20_TuneZ2/*Counters.root");
+  chains[4]->Add("results/ZPYTHIA/DYToMuMu_M-10To20_TuneZ2/*Counters.root");
+  chains[5]->Add("results/ZPYTHIA/DYToTauTau_M-10To20_TuneZ2/*Counters.root");
 
-  chains[6]->Add("results/SingleTop/TToBLNu_TuneZ2_s-channel_7TeV-madgraph/*Counters.root");
-  chains[7]->Add("results/SingleTop/TToBLNu_TuneZ2_t-channel_7TeV-madgraph/*Counters.root");
-  chains[8]->Add("results/SingleTop/TToBLNu_TuneZ2_tW-channel_7TeV-madgraph/*Counters.root");
+  chains[6]->Add("results/ZPYTHIA/DYToEE_M-20_CT10_TuneZ2_PU/*Counters.root");
+  chains[7]->Add("results/ZPYTHIA/DYToMuMu_M-20_CT10_TuneZ2_PU/*Counters.root");
+  chains[8]->Add("results/ZPYTHIA/DYToTauTau_M-20_CT10_TuneZ2_7TeV-powheg-pythia-tauola/*Counters.root");
 
-  chains[9]->Add("results/TTbar/TTJets_TuneD6T/*Counters.root");
+  chains[9]->Add("results/SingleTop/TToBLNu_TuneZ2_s-channel_7TeV-madgraph/*Counters.root");
+  chains[10]->Add("results/SingleTop/TToBLNu_TuneZ2_t-channel_7TeV-madgraph/*Counters.root");
+  chains[11]->Add("results/SingleTop/TToBLNu_TuneZ2_tW-channel_7TeV-madgraph/*Counters.root");
 
-  chains[10]->Add("results/HiggsWW/GluGluToHToWWTo2L2Nu_M-130/*Counters.root");
+  chains[12]->Add("results/TTbar/TTJets_TuneD6T/*Counters.root");
+
+  chains[13]->Add("results/HiggsWW/GluGluToHToWWTo2L2Nu_M-130/*Counters.root");
 
   //  chains[34]->Add("results/DiBosons/Wgamma/*Counters.root");
-  chains[11]->Add("results/DiBosons/WWTo2L2Nu_TuneZ2/*Counters.root");
-  chains[12]->Add("results/DiBosons/WZTo3LNu_TuneZ2/*Counters.root");
-  chains[13]->Add("results/DiBosons/ZZtoAnything_TuneZ2/*Counters.root");
+  chains[14]->Add("results/DiBosons/WWTo2L2Nu_TuneZ2/*Counters.root");
+  chains[15]->Add("results/DiBosons/WZTo3LNu_TuneZ2/*Counters.root");
+  chains[16]->Add("results/DiBosons/ZZtoAnything_TuneZ2/*Counters.root");
 
   cout << "chains added. " << endl;
 
@@ -48,6 +52,10 @@ void countEvents() {
   sampleName.push_back("W->enu");
   sampleName.push_back("W->munu");
   sampleName.push_back("W->taunu");
+
+  sampleName.push_back("DY(ee) 10<m<20 GeV");
+  sampleName.push_back("DY(mm) 10<m<20 GeV");
+  sampleName.push_back("DY(tautau) 10<m<20 GeV");
 
   sampleName.push_back("DY(ee) m>20 GeV");
   sampleName.push_back("DY(mm) m>20 GeV");
@@ -66,12 +74,12 @@ void countEvents() {
   sampleName.push_back("WZ");
   sampleName.push_back("ZZ");
 
-  float nEv[14];
-  for(int isample=0; isample<14; isample++) {
+  float nEv[17];
+  for(int isample=0; isample<17; isample++) {
     nEv[isample] = 0.0;
   }
 
-  for(int isample=0; isample<14; isample++) {
+  for(int isample=0; isample<17; isample++) {
 
     cout << "\tProcessing sample # " << isample << "..." << endl;
 
@@ -97,7 +105,7 @@ void countEvents() {
     }
   }
 
-  for(int isample=0; isample<14; isample++) {
+  for(int isample=0; isample<17; isample++) {
     cout << "Events processed for sample: " << sampleName[isample] << " = " << nEv[isample] << endl;
   }
   
