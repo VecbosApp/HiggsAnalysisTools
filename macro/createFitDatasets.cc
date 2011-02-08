5d9336874f7dde694252e43a66e907a8132c6949
emanuele <>
2011-02-08 11:04:32 +0000

    - remove the not finalSelection events - remove useless variables - add the finalstate bit

--- macro/createFitDatasets.cc
+++ macro/createFitDatasets.cc
@@ -19,12 +19,13 @@ void createAllData() {
   createSingleDataset("results_data/merged/dataset_eg_ee.root","results_data/datasets/dataset_eg_ee.root");
 
   cout << "CREATING ROODATASET ON DATA FOR MM CHANNEL..." << endl;
-  cout << "WARNING: NOT PRODUCING ANYTHING: MISSING DATASET." << endl;
-  //createSingleDataset("results_data/merged/dataset_mu_mm.root","results_data/datasets/dataset_mu_mm.root");
+  createSingleDataset("results_data/merged/dataset_mu_mm.root","results_data/datasets/dataset_mu_mm.root");
 
   cout << "CREATING ROODATASET ON DATA FOR EM CHANNEL..." << endl;
-  cout << "WARNING: using only eg dataset for the moment." << endl;
-  createSingleDataset("results_data/merged/dataset_eg_em.root","results_data/datasets/dataset_eg_em.root");
+  createSingleDataset("results_data/merged/dataset_muAndEg_em.root","results_data/datasets/dataset_muAndEg_em.root");
+
+  cout << "CREATING ROODATASET ON DATA FOR ALL CHANNELS TOGETHER..." << endl;
+  createSingleDataset("results_data/merged/dataset_muAndEg_ll.root","results_data/datasets/dataset_muAndEg_ll.root");
 
 }
 
@@ -54,16 +55,28 @@ void createSingleDataset(const char *treefile, const char *roofitfile) {
 
   cout << "roofitting file " << treefile << " in " << roofitfile << endl;
 
-  RooRealVar *jetcat = new RooRealVar("jetcat","jetcat",-1,1); // cut the -2 ( >1 jet )
-  RooRealVar *met    = new RooRealVar("met","E_{T}^{miss}",0,200,"GeV");
+  RooRealVar *jetcat = new RooRealVar("jetcat","jetcat",0,10); // cut the -2 ( >1 jet )
+  RooRealVar *projmet  = new RooRealVar("projMet","proj. E_{T}^{miss}",0,250,"GeV");
   RooRealVar *deltaPhi = new RooRealVar("deltaPhi","#Delta#phi",0,180,"#deg");
-  RooRealVar *maxPtEle = new RooRealVar("maxPtEle","p_{T}^{max}",20,200,"GeV");
-  RooRealVar *eleInvMass = new RooRealVar("eleInvMass","m(l^{+}l^{-})",12,150,"GeV");
-  RooRealVar *bTagImpPar = new RooRealVar("bTagImpPar","b-tag",-1001.,2.0);
-  RooRealVar *weight = new RooRealVar("weight","weight",0,10000);
-  RooRealVar *event = new RooRealVar("event","progressive event number",0,1e+30);
+  RooRealVar *exCosDphi = new RooRealVar("expCosDphi","e^{cos(#Delta#phi)}",exp(-1.),exp(1.));
+  RooRealVar *maxPtEle = new RooRealVar("maxPtEle","p_{T}^{max}",0,150,"GeV");
+  RooRealVar *eleInvMass = new RooRealVar("eleInvMass","m(l^{+}l^{-})",0,90,"GeV"); // <---- put the cut in the sel! (otherwise maxPtEle has two bums for </> Z veto cut)
+  RooRealVar *bTagImpPar = new RooRealVar("bTagImpPar","b-tag",-1e+30,1e+30);
+  RooRealVar *weight = new RooRealVar("weight","weight",0,1e+30);
+  RooRealVar *consecevent = new RooRealVar("consecevent","progressive event number",0,1e+30);
+  RooRealVar *finalSelection = new RooRealVar("finalSelection","finalSelection",1,1); // <--- only the final selected events!
 
-  RooArgSet setHiggs(*jetcat,*met,*deltaPhi,*maxPtEle,*eleInvMass,*bTagImpPar,*weight,*event);
+  // run variables
+  RooRealVar *run = new RooRealVar("run","run number",-1e+32,1e+32);
+  RooRealVar *lumi = new RooRealVar("lumi","lumi section number",-1e+32,1e+32);
+  RooRealVar *event = new RooRealVar("event","event number",-1e+32,1e+32);
+
+  // finalstate: 0=ee/1=mm/2=em
+  RooRealVar *finalstate = new RooRealVar("finalstate","finalstate",-1,3);
+
+  RooArgSet setHiggs(*jetcat,*projmet,*deltaPhi,*maxPtEle,*eleInvMass,*bTagImpPar,*weight,*consecevent);
+  RooArgSet setHiggs1(*finalSelection,*exCosDphi,*run,*lumi,*event,*finalstate);
+  setHiggs.add(setHiggs1);
 
   TFile *file = TFile::Open(treefile);
   TTree *tree = (TTree*)file->Get("T1");
