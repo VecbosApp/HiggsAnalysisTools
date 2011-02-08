8325121f81ccaba60d0a7be93606d3847467bf95
emanuele <>
2011-02-08 11:02:30 +0000

    add the finalstate

--- macro/weightTrees.sh
+++ macro/weightTrees.sh
@@ -6,28 +6,31 @@
 # usage: ./weightTrees.sh
 
 # PS. weights are compute for 1000 pb-1
-mass=$1
+lumi=$1
 
-echo "Adding weights for ee datasets..."
+echo "Adding weights for ee datasets for " $lumi " pb-1..."
 root -l -b <<EOF
 
 .L addWeightsToTree.cc+
 
-addWeights("results/merged/H130_ee.root", 4.11709e-06);
-addWeights("results/merged/TTbar_ee.root", 8.20589e-05);
-addWeights("results/merged/SingleTop_sChannel_ee.root", 2.54689e-06);
-addWeights("results/merged/SingleTop_tChannel_ee.root", 2.25913e-05);
-addWeights("results/merged/SingleTop_tWChannel_ee.root", 6.93873e-06);
-addWeights("results/merged/Zee_ee.root", 0.000813084); 
-addWeights("results/merged/Zmm_ee.root", 0.000877336); 
-addWeights("results/merged/Ztautau_ee.root", 0.000901408); 
-addWeights("results/merged/Wenu_ee.root", 0.00276752);
-addWeights("results/merged/Wmunu_ee.root", 0.00145284);
-addWeights("results/merged/Wtaunu_ee.root", 0.00197614);
-addWeights("results/merged/WW_ee.root", 4.09406e-05);
-addWeights("results/merged/WZ_ee.root", 5.44947e-06);
-addWeights("results/merged/ZZ_ee.root", 1.89505e-06);
-
+addWeights("results/merged/H160_ee.root", 2.695590e-06*$lumi, 0);
+addWeights("results/merged/WW_ee.root", 4.094063e-05*$lumi, 0);
+addWeights("results/merged/ggWW_ee.root", 1.538923e-05*$lumi, 0);
+addWeights("results/merged/WZ_ee.root", 5.449473e-06*$lumi, 0);
+addWeights("results/merged/ZZ_ee.root", 2.791750e-06*$lumi, 0);
+addWeights("results/merged/Wenu_ee.root", 4.737725e-03*$lumi, 0);
+addWeights("results/merged/Wmunu_ee.root", 1.452839e-03*$lumi, 0);
+addWeights("results/merged/Wtaunu_ee.root", 1.976142e-03*$lumi, 0);
+addWeights("results/merged/Zee_Hi_ee.root", 8.334209e-04*$lumi, 0);
+addWeights("results/merged/Zmm_Hi_ee.root", 8.773362e-04*$lumi, 0);
+addWeights("results/merged/Ztautau_Hi_ee.root", 9.014079e-04*$lumi, 0);
+addWeights("results/merged/Zee_Lo_ee.root", 4.540642e-01*$lumi, 0);
+addWeights("results/merged/Zmm_Lo_ee.root", 1.795360e-02*$lumi, 0);
+addWeights("results/merged/Ztautau_Lo_ee.root", 4.565363e-04*$lumi, 0);
+addWeights("results/merged/TTbar_ee.root", 1.313869e-04*$lumi, 0);
+addWeights("results/merged/SingleTop_sChannel_ee.root", 3.065486e-06*$lumi, 0);
+addWeights("results/merged/SingleTop_tChannel_ee.root", 4.323927e-05*$lumi, 0);
+addWeights("results/merged/SingleTop_tWChannel_ee.root", 6.938728e-06*$lumi, 0);
 .q
 
 EOF
@@ -37,20 +40,24 @@ root -l -b <<EOF
 
 .L addWeightsToTree.cc+
 
-addWeights("results/merged/H130_mm.root", 4.11709e-06);
-addWeights("results/merged/TTbar_mm.root", 8.20589e-05);
-addWeights("results/merged/SingleTop_sChannel_mm.root", 2.54689e-06);
-addWeights("results/merged/SingleTop_tChannel_mm.root", 2.25913e-05);
-addWeights("results/merged/SingleTop_tWChannel_mm.root", 6.93873e-06);
-addWeights("results/merged/Zee_mm.root", 0.000813084); 
-addWeights("results/merged/Zmm_mm.root", 0.000877336); 
-addWeights("results/merged/Ztautau_mm.root", 0.000901408); 
-addWeights("results/merged/Wenu_mm.root", 0.00276752);
-addWeights("results/merged/Wmunu_mm.root", 0.00145284);
-addWeights("results/merged/Wtaunu_mm.root", 0.00197614);
-addWeights("results/merged/WW_mm.root", 4.09406e-05);
-addWeights("results/merged/WZ_mm.root", 5.44947e-06);
-addWeights("results/merged/ZZ_mm.root", 1.89505e-06);
+addWeights("results/merged/H160_mm.root", 2.695590e-06*$lumi, 1);
+addWeights("results/merged/WW_mm.root", 4.094063e-05*$lumi, 1);
+addWeights("results/merged/ggWW_mm.root", 1.538923e-05*$lumi, 1);
+addWeights("results/merged/WZ_mm.root", 5.449473e-06*$lumi, 1);
+addWeights("results/merged/ZZ_mm.root", 2.791750e-06*$lumi, 1);
+addWeights("results/merged/Wenu_mm.root", 4.737725e-03*$lumi, 1);
+addWeights("results/merged/Wmunu_mm.root", 1.452839e-03*$lumi, 1);
+addWeights("results/merged/Wtaunu_mm.root", 1.976142e-03*$lumi, 1);
+addWeights("results/merged/Zee_Hi_mm.root", 8.334209e-04*$lumi, 1);
+addWeights("results/merged/Zmm_Hi_mm.root", 8.773362e-04*$lumi, 1);
+addWeights("results/merged/Ztautau_Hi_mm.root", 9.014079e-04*$lumi, 1);
+addWeights("results/merged/Zee_Lo_mm.root", 4.540642e-01*$lumi, 1);
+addWeights("results/merged/Zmm_Lo_mm.root", 1.795360e-02*$lumi, 1);
+addWeights("results/merged/Ztautau_Lo_mm.root", 4.565363e-04*$lumi, 1);
+addWeights("results/merged/TTbar_mm.root", 1.313869e-04*$lumi, 1);
+addWeights("results/merged/SingleTop_sChannel_mm.root", 3.065486e-06*$lumi, 1);
+addWeights("results/merged/SingleTop_tChannel_mm.root", 4.323927e-05*$lumi, 1);
+addWeights("results/merged/SingleTop_tWChannel_mm.root", 6.938728e-06*$lumi, 1);
 
 .q
 
@@ -62,22 +69,26 @@ root -l -b <<EOF
 
 .L addWeightsToTree.cc+
 
-addWeights("results/merged/H130_em.root", 4.11709e-06);
-addWeights("results/merged/TTbar_em.root", 8.20589e-05);
-addWeights("results/merged/SingleTop_sChannel_em.root", 2.54689e-06);
-addWeights("results/merged/SingleTop_tChannel_em.root", 2.25913e-05);
-addWeights("results/merged/SingleTop_tWChannel_em.root", 6.93873e-06);
-addWeights("results/merged/Zee_em.root", 0.000813084); 
-addWeights("results/merged/Zmm_em.root", 0.000877336); 
-addWeights("results/merged/Ztautau_em.root", 0.000901408); 
-addWeights("results/merged/Wenu_em.root", 0.00276752);
-addWeights("results/merged/Wmunu_em.root", 0.00145284);
-addWeights("results/merged/Wtaunu_em.root", 0.00197614);
-addWeights("results/merged/WW_em.root", 4.09406e-05);
-addWeights("results/merged/WZ_em.root", 5.44947e-06);
-addWeights("results/merged/ZZ_em.root", 1.89505e-06);
+addWeights("results/merged/H160_em.root", 2.695590e-06*$lumi, 2);
+addWeights("results/merged/WW_em.root", 4.094063e-05*$lumi, 2);
+addWeights("results/merged/ggWW_em.root", 1.538923e-05*$lumi, 2);
+addWeights("results/merged/WZ_em.root", 5.449473e-06*$lumi, 2);
+addWeights("results/merged/ZZ_em.root", 2.791750e-06*$lumi, 2);
+addWeights("results/merged/Wenu_em.root", 4.737725e-03*$lumi, 2);
+addWeights("results/merged/Wmunu_em.root", 1.452839e-03*$lumi, 2);
+addWeights("results/merged/Wtaunu_em.root", 1.976142e-03*$lumi, 2);
+addWeights("results/merged/Zee_Hi_em.root", 8.334209e-04*$lumi, 2);
+addWeights("results/merged/Zmm_Hi_em.root", 8.773362e-04*$lumi, 2);
+addWeights("results/merged/Ztautau_Hi_em.root", 9.014079e-04*$lumi, 2);
+addWeights("results/merged/Zee_Lo_em.root", 4.540642e-01*$lumi, 2);
+addWeights("results/merged/Zmm_Lo_em.root", 1.795360e-02*$lumi, 2);
+addWeights("results/merged/Ztautau_Lo_em.root", 4.565363e-04*$lumi, 2);
+addWeights("results/merged/TTbar_em.root", 1.313869e-04*$lumi, 2);
+addWeights("results/merged/SingleTop_sChannel_em.root", 3.065486e-06*$lumi, 2);
+addWeights("results/merged/SingleTop_tChannel_em.root", 4.323927e-05*$lumi, 2);
+addWeights("results/merged/SingleTop_tWChannel_em.root", 6.938728e-06*$lumi, 2);
 
-.q
+.Q
 
 EOF
 
