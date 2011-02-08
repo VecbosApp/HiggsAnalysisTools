b2d261b8bff423a432975328aad06b91bd2e58c5
emanuele <>
2011-02-08 11:07:17 +0000

    include the low mass Z samples

--- macro/mergeMultiSamples.sh
+++ macro/mergeMultiSamples.sh
@@ -15,7 +15,7 @@ cp results/merged/WW_ee.root results/datasets_trees
 hadd results/datasets_trees/top_ee.root results/merged/TTbar_ee.root results/merged/SingleTop_sChannel_ee.root results/merged/SingleTop_tChannel_ee.root results/merged/SingleTop_tWChannel_ee.root 
 
 # merging all other backgrounds
-hadd results/datasets_trees/others_ee.root results/merged/Wenu_ee.root results/merged/Wmunu_ee.root results/merged/Wtaunu_ee.root results/merged/Zee_ee.root results/merged/Zmm_ee.root results/merged/Ztautau_ee.root results/merged/WZ_ee.root results/merged/ZZ_ee.root
+hadd results/datasets_trees/others_ee.root results/merged/Wenu_ee.root results/merged/Wmunu_ee.root results/merged/Wtaunu_ee.root results/merged/Zee_*_ee.root results/merged/Zmm_*_ee.root results/merged/Ztautau_*_ee.root results/merged/WZ_ee.root results/merged/ZZ_ee.root
 
 
 echo "Now merging species for mm..."
@@ -29,7 +29,7 @@ cp results/merged/WW_mm.root results/datasets_trees
 hadd results/datasets_trees/top_mm.root results/merged/TTbar_mm.root results/merged/SingleTop_sChannel_mm.root results/merged/SingleTop_tChannel_mm.root results/merged/SingleTop_tWChannel_mm.root 
 
 # merging all other backgrounds
-hadd results/datasets_trees/others_mm.root results/merged/Wenu_mm.root results/merged/Wmunu_mm.root results/merged/Wtaunu_mm.root results/merged/Zee_mm.root results/merged/Zmm_mm.root results/merged/Ztautau_mm.root results/merged/WZ_mm.root results/merged/ZZ_mm.root
+hadd results/datasets_trees/others_mm.root results/merged/Wenu_mm.root results/merged/Wmunu_mm.root results/merged/Wtaunu_mm.root results/merged/Zee_*_mm.root results/merged/Zmm_*_mm.root results/merged/Ztautau_*_mm.root results/merged/WZ_mm.root results/merged/ZZ_mm.root
 
 
 
@@ -44,4 +44,4 @@ cp results/merged/WW_em.root results/datasets_trees
 hadd results/datasets_trees/top_em.root results/merged/TTbar_em.root results/merged/SingleTop_sChannel_em.root results/merged/SingleTop_tChannel_em.root results/merged/SingleTop_tWChannel_em.root 
 
 # merging all other backgrounds
-hadd results/datasets_trees/others_em.root results/merged/Wenu_em.root results/merged/Wmunu_em.root results/merged/Wtaunu_em.root results/merged/Zee_em.root results/merged/Zmm_em.root results/merged/Ztautau_em.root results/merged/WZ_em.root results/merged/ZZ_em.root
+hadd results/datasets_trees/others_em.root results/merged/Wenu_em.root results/merged/Wmunu_em.root results/merged/Wtaunu_em.root results/merged/Zee_*_em.root results/merged/Zmm_*_em.root results/merged/Ztautau_*_em.root results/merged/WZ_em.root results/merged/ZZ_em.root
