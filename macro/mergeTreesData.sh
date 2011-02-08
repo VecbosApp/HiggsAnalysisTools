2ac572f6f3bda99164b682eaf78eefff12540e60
emanuele <>
2011-02-08 10:57:51 +0000

    move to Nov4 rereco datasets

--- macro/mergeTreesData.sh
+++ macro/mergeTreesData.sh
@@ -11,17 +11,17 @@ mkdir -p results_data/merged
 if [ "$dataset" = "eg" ]; then
 
     echo "Now merging EE datasets..."
-    hadd results_data/merged/dataset_eg_ee.root results_data/dataset_eg_Sep3rdReReco/*datasetEE.root results_data/PDElectron_11pbTo40pb/*datasetEE.root
+    hadd results_data/merged/dataset_eg_ee.root results_data/dataset_eg_Nov4ReReco_PU/*datasetEE.root
 
     echo "Now merging EM datasets..."
-    hadd results_data/merged/dataset_eg_em.root results_data/dataset_eg_Sep3rdReReco/*datasetEM.root results_data/PDElectron_11pbTo40pb/*datasetEM.root
+    hadd results_data/merged/dataset_eg_em.root results_data/dataset_eg_Nov4ReReco_PU/*datasetEM.root
 elif [ "$dataset" = "mu" ]; then
 
     echo "Now merging MM datasets..."
-    hadd results_data/merged/dataset_mu_mm.root results_data/Data7TeV/dataset_mu/*datasetMM.root
+    hadd results_data/merged/dataset_mu_mm.root results_data/dataset_mu_Nov4ReReco_PU/*datasetMM.root
 
     echo "Now merging EM datasets..."
-    hadd results_data/merged/dataset_mu_em.root results_data/Data7TeV/dataset_mu/*datasetEM.root
+    hadd results_data/merged/dataset_mu_em.root results_data/dataset_mu_Nov4ReReco_PU/*datasetEM.root
 else
     echo "dataset can be only eg or mu. Nothing done."
 fi
