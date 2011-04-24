2f22b83cf8006916bb94c73bf8220024d3c840af
emanuele <>
2011-04-24 14:00:43 +0000

    - fix repetition of prefix - adapt for 4.1.X release

--- cmst3_submit_manyfilesperjob.py
+++ cmst3_submit_manyfilesperjob.py
@@ -20,18 +20,18 @@ application = sys.argv[4]
 prefix = sys.argv[6]
 isMC = int(sys.argv[7])
 if isMC != 0:
-    inputlist = "cmst3_38X/MC/"+process+"/"+dataset+".list"
+    inputlist = "cmst3_41X/MC/"+process+"/"+dataset+".list"
 else:
-    inputlist = "cmst3_38X/"+process+"/"+dataset+".list"
+    inputlist = "cmst3_41X/"+process+"/"+dataset+".list"
 # to write on the cmst3 cluster disks
 ################################################
 #castordir = "/castor/cern.ch/user/m/mpierini/CMST3/Vecbos/output/"
 #outputmain = castordir+output
 # to write on local disks
 ################################################
-castordir = "/castor/cern.ch/user/e/emanuele/XXX";
-diskoutputdir = "/cmsrm/pc21_2/emanuele/data/Higgs3.9.X/"+prefix+"/"
-outputmain = castordir+"/"+process+"/"+output
+castordir = "/castor/cern.ch/user/e/emanuele/Higgs4.1.X/"
+diskoutputdir = "/cmsrm/pc23_2/emanuele/data/Higgs4.1.X/"
+outputmain = castordir+"/"+prefix+"/"+process+"/"+output
 diskoutputmain = diskoutputdir+"/"+prefix+"/"+process+"/"+output
 # prepare job to write on the cmst3 cluster disks
 ################################################
@@ -48,7 +48,7 @@ if castordir != "none":
 else: os.system("mkdir -p "+outputroot)
 
 if diskoutputdir != "none": 
-    os.system("ssh -o BatchMode=yes -o StrictHostKeyChecking=no pccmsrm18 mkdir -p "+diskoutputmain)
+    os.system("ssh -o BatchMode=yes -o StrictHostKeyChecking=no pccmsrm23 mkdir -p "+diskoutputmain)
 
 #look for the current directory
 #######################################
@@ -81,16 +81,19 @@ while (len(inputfiles) > 0):
     #    outputfile.write('cd '+pwd)
     outputfile.write('cp '+pwd+'/pdfs_MC.root $WORKDIR\n')
     outputfile.write('cp -r '+pwd+"/"+prefix+'/config $WORKDIR\n')
+    outputfile.write('export SCRAM_ARCH=slc5_amd64_gcc434\n')
+    outputfile.write('cd /afs/cern.ch/user/e/emanuele/scratch0/higgs/CMSSW_4_1_3/\n')
+    outputfile.write('eval `scramv1 runtime -sh`\n')
     outputfile.write('cd $WORKDIR\n')
     outputfile.write(pwd+'/'+application+' '+inputfilename+" "+output+"_"+str(ijob)+"_ "+"\n")
 
 #    if castordir != "none": outputfile.write('./VecbosApp '+inputfilename+" "+" rfio://"+outputroot+output+"_"+str(ijob)+".root\n")
 #    else:  
     outputfile.write('ls *.root | grep -v Z_calibFall08 | xargs -i rfcp {} '+outputroot+'\n')
-    outputfile.write('ls *.root | grep -v Z_calibFall08 | xargs -i scp -o BatchMode=yes -o StrictHostKeyChecking=no {} pccmsrm18:'+diskoutputmain+'/{}\n') 
+    outputfile.write('ls *.root | grep -v Z_calibFall08 | xargs -i scp -o BatchMode=yes -o StrictHostKeyChecking=no {} pccmsrm23:'+diskoutputmain+'/{}\n') 
     outputfile.close
     os.system("echo bsub -q "+queue+" -o "+prefix+"/"+output+"/log/"+output+"_"+str(ijob)+".log source "+pwd+"/"+outputname)
     os.system("bsub -q "+queue+" -o "+prefix+"/"+process+"/"+output+"/log/"+output+"_"+str(ijob)+".log source "+pwd+"/"+outputname+" -copyInput="+process+"_"+str(ijob))
     ijob = ijob+1
-    time.sleep(2)
+    time.sleep(1)
     continue
