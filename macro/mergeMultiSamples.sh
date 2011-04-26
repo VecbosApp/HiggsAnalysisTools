a00120b87f41f90101556937e4947005d344d4e5
emanuele <>
2011-04-26 14:00:05 +0000

    adding me

--- macro/mergeMultiSamples.sh
+++ macro/mergeMultiSamples.sh
@@ -45,3 +45,19 @@ hadd results/datasets_trees/top_em.root results/merged/TTbar_em.root results/mer
 
 # merging all other backgrounds
 hadd results/datasets_trees/others_em.root results/merged/Wenu_em.root results/merged/Wmunu_em.root results/merged/Wtaunu_em.root results/merged/Zee_*_em.root results/merged/Zmm_*_em.root results/merged/Ztautau_*_em.root results/merged/WZ_em.root results/merged/ZZ_em.root
+
+
+
+echo "Now merging species for me..."
+# signal is always a species per se
+cp results/merged/H*_me.root results/datasets_trees
+
+# WW is a species per se
+cp results/merged/WW_me.root results/datasets_trees
+
+# merging ttbar and single t in a species
+hadd results/datasets_trees/top_me.root results/merged/TTbar_me.root results/merged/SingleTop_sChannel_me.root results/merged/SingleTop_tChannel_me.root results/merged/SingleTop_tWChannel_me.root 
+
+# merging all other backgrounds
+hadd results/datasets_trees/others_me.root results/merged/Wenu_me.root results/merged/Wmunu_me.root results/merged/Wtaunu_me.root results/merged/Zee_*_me.root results/merged/Zmm_*_me.root results/merged/Ztautau_*_me.root results/merged/WZ_me.root results/merged/ZZ_me.root
+
