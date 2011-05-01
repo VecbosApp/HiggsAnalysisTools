0308316390a90e8b1b890788e99e4f347eed384a
emanuele <>
2011-05-01 22:41:23 +0000

    - move to W+jets madgraph - add Wgamma

--- macro/mergeTrees.sh
+++ macro/mergeTrees.sh
@@ -12,9 +12,7 @@ hadd results/merged/TTbar_ee.root results/Spring11/TTJets_TuneZ2_7TeV-madgraph-t
 hadd results/merged/SingleTop_sChannel_ee.root results/Spring11/TToBLNu_TuneZ2_s-channel_7TeV-madgraph/*datasetEE.root
 hadd results/merged/SingleTop_tChannel_ee.root results/Spring11/TToBLNu_TuneZ2_t-channel_7TeV-madgraph/*datasetEE.root
 hadd results/merged/SingleTop_tWChannel_ee.root results/Spring11/TToBLNu_TuneZ2_tW-channel_7TeV-madgraph/*datasetEE.root
-hadd results/merged/Wenu_ee.root results/Spring11/WToENu_TuneZ2_7TeV-pythia6/*datasetEE.root
-hadd results/merged/Wmunu_ee.root results/Spring11/WToMuNu_TuneZ2_7TeV-pythia6/*datasetEE.root
-hadd results/merged/Wtaunu_ee.root results/Spring11/WToTauNu_TuneZ2_7TeV-pythia6-tauola/*datasetEE.root
+hadd results/merged/Wjets_ee.root results/Spring11/WJetsToLNu_TuneZ2_7TeV-madgraph-tauola/*datasetEE.root
 hadd results/merged/Zee_Hi_ee.root results/Spring11/DYToEE_M-20_TuneZ2_7TeV-pythia6/*datasetEE.root
 hadd results/merged/Zmm_Hi_ee.root results/Spring11/DYToMuMu_M-20_TuneZ2_7TeV-pythia6/*datasetEE.root
 hadd results/merged/Ztautau_Hi_ee.root results/Spring11/DYToTauTau_M-20_TuneZ2_7TeV-pythia6-tauola/*datasetEE.root
@@ -25,7 +23,7 @@ hadd results/merged/WW_ee.root results/Spring11/WWTo2L2Nu_TuneZ2_7TeV-pythia6/*d
 hadd results/merged/ggWW_ee.root results/Spring11/GluGluToWWTo4L_TuneZ2_7TeV-gg2ww-pythia6/*datasetEE.root
 hadd results/merged/WZ_ee.root results/Spring11/WZTo3LNu_TuneZ2_7TeV-pythia6/*datasetEE.root
 hadd results/merged/ZZ_ee.root results/Spring11/ZZtoAnything_TuneZ2_7TeV-pythia6-tauola/*datasetEE.root
-#hadd results/merged/Wgamma_ee.root results/Spring11/Wgamma/*datasetEE.root
+hadd results/merged/Wgamma_ee.root results/Spring11/PhotonVJets_7TeV-madgraph/*datasetEE.root
 
 echo "Now merging MM datasets..."
 hadd results/merged/H$Hmass\_mm.root results/Spring11/GluGluToHToWWTo2L2Nu_M-$Hmass\_7TeV-powheg-pythia6/*datasetMM.root
@@ -33,9 +31,7 @@ hadd results/merged/TTbar_mm.root results/Spring11/TTJets_TuneZ2_7TeV-madgraph-t
 hadd results/merged/SingleTop_sChannel_mm.root results/Spring11/TToBLNu_TuneZ2_s-channel_7TeV-madgraph/*datasetMM.root
 hadd results/merged/SingleTop_tChannel_mm.root results/Spring11/TToBLNu_TuneZ2_t-channel_7TeV-madgraph/*datasetMM.root
 hadd results/merged/SingleTop_tWChannel_mm.root results/Spring11/TToBLNu_TuneZ2_tW-channel_7TeV-madgraph/*datasetMM.root
-hadd results/merged/Wenu_mm.root results/Spring11/WToENu_TuneZ2_7TeV-pythia6/*datasetMM.root
-hadd results/merged/Wmunu_mm.root results/Spring11/WToMuNu_TuneZ2_7TeV-pythia6/*datasetMM.root
-hadd results/merged/Wtaunu_mm.root results/Spring11/WToTauNu_TuneZ2_7TeV-pythia6-tauola/*datasetMM.root
+hadd results/merged/Wjets_mm.root results/Spring11/WJetsToLNu_TuneZ2_7TeV-madgraph-tauola/*datasetMM.root
 hadd results/merged/Zee_Hi_mm.root results/Spring11/DYToEE_M-20_TuneZ2_7TeV-pythia6/*datasetMM.root
 hadd results/merged/Zmm_Hi_mm.root results/Spring11/DYToMuMu_M-20_TuneZ2_7TeV-pythia6/*datasetMM.root
 hadd results/merged/Ztautau_Hi_mm.root results/Spring11/DYToTauTau_M-20_TuneZ2_7TeV-pythia6-tauola/*datasetMM.root
@@ -46,7 +42,7 @@ hadd results/merged/WW_mm.root results/Spring11/WWTo2L2Nu_TuneZ2_7TeV-pythia6/*d
 hadd results/merged/ggWW_mm.root results/Spring11/GluGluToWWTo4L_TuneZ2_7TeV-gg2ww-pythia6/*datasetMM.root
 hadd results/merged/WZ_mm.root results/Spring11/WZTo3LNu_TuneZ2_7TeV-pythia6/*datasetMM.root
 hadd results/merged/ZZ_mm.root results/Spring11/ZZtoAnything_TuneZ2_7TeV-pythia6-tauola/*datasetMM.root
-#hadd results/merged/Wgamma_mm.root results/Spring11/Wgamma/*datasetMM.root
+hadd results/merged/Wgamma_mm.root results/Spring11/PhotonVJets_7TeV-madgraph/*datasetMM.root
 
 echo "Now merging EM datasets..."
 hadd results/merged/H$Hmass\_em.root results/Spring11/GluGluToHToWWTo2L2Nu_M-$Hmass\_7TeV-powheg-pythia6/*datasetEM.root
@@ -54,9 +50,7 @@ hadd results/merged/TTbar_em.root results/Spring11/TTJets_TuneZ2_7TeV-madgraph-t
 hadd results/merged/SingleTop_sChannel_em.root results/Spring11/TToBLNu_TuneZ2_s-channel_7TeV-madgraph/*datasetEM.root
 hadd results/merged/SingleTop_tChannel_em.root results/Spring11/TToBLNu_TuneZ2_t-channel_7TeV-madgraph/*datasetEM.root
 hadd results/merged/SingleTop_tWChannel_em.root results/Spring11/TToBLNu_TuneZ2_tW-channel_7TeV-madgraph/*datasetEM.root
-hadd results/merged/Wenu_em.root results/Spring11/WToENu_TuneZ2_7TeV-pythia6/*datasetEM.root
-hadd results/merged/Wmunu_em.root results/Spring11/WToMuNu_TuneZ2_7TeV-pythia6/*datasetEM.root
-hadd results/merged/Wtaunu_em.root results/Spring11/WToTauNu_TuneZ2_7TeV-pythia6-tauola/*datasetEM.root
+hadd results/merged/Wjets_em.root results/Spring11/WJetsToLNu_TuneZ2_7TeV-madgraph-tauola/*datasetEM.root
 hadd results/merged/Zee_Hi_em.root results/Spring11/DYToEE_M-20_TuneZ2_7TeV-pythia6/*datasetEM.root
 hadd results/merged/Zmm_Hi_em.root results/Spring11/DYToMuMu_M-20_TuneZ2_7TeV-pythia6/*datasetEM.root
 hadd results/merged/Ztautau_Hi_em.root results/Spring11/DYToTauTau_M-20_TuneZ2_7TeV-pythia6-tauola/*datasetEM.root
@@ -67,7 +61,7 @@ hadd results/merged/WW_em.root results/Spring11/WWTo2L2Nu_TuneZ2_7TeV-pythia6/*d
 hadd results/merged/ggWW_em.root results/Spring11/GluGluToWWTo4L_TuneZ2_7TeV-gg2ww-pythia6/*datasetEM.root
 hadd results/merged/WZ_em.root results/Spring11/WZTo3LNu_TuneZ2_7TeV-pythia6/*datasetEM.root
 hadd results/merged/ZZ_em.root results/Spring11/ZZtoAnything_TuneZ2_7TeV-pythia6-tauola/*datasetEM.root
-#hadd results/merged/Wgamma_em.root results/Spring11/Wgamma/*datasetEM.root
+hadd results/merged/Wgamma_em.root results/Spring11/PhotonVJets_7TeV-madgraph/*datasetEM.root
 
 echo "Now merging ME datasets..."
 hadd results/merged/H$Hmass\_me.root results/Spring11/GluGluToHToWWTo2L2Nu_M-$Hmass\_7TeV-powheg-pythia6/*datasetME.root
@@ -75,9 +69,7 @@ hadd results/merged/TTbar_me.root results/Spring11/TTJets_TuneZ2_7TeV-madgraph-t
 hadd results/merged/SingleTop_sChannel_me.root results/Spring11/TToBLNu_TuneZ2_s-channel_7TeV-madgraph/*datasetME.root
 hadd results/merged/SingleTop_tChannel_me.root results/Spring11/TToBLNu_TuneZ2_t-channel_7TeV-madgraph/*datasetME.root
 hadd results/merged/SingleTop_tWChannel_me.root results/Spring11/TToBLNu_TuneZ2_tW-channel_7TeV-madgraph/*datasetME.root
-hadd results/merged/Wenu_me.root results/Spring11/WToENu_TuneZ2_7TeV-pythia6/*datasetME.root
-hadd results/merged/Wmunu_me.root results/Spring11/WToMuNu_TuneZ2_7TeV-pythia6/*datasetME.root
-hadd results/merged/Wtaunu_me.root results/Spring11/WToTauNu_TuneZ2_7TeV-pythia6-tauola/*datasetME.root
+hadd results/merged/Wjets_me.root results/Spring11/WJetsToLNu_TuneZ2_7TeV-madgraph-tauola/*datasetME.root
 hadd results/merged/Zee_Hi_me.root results/Spring11/DYToEE_M-20_TuneZ2_7TeV-pythia6/*datasetME.root
 hadd results/merged/Zmm_Hi_me.root results/Spring11/DYToMuMu_M-20_TuneZ2_7TeV-pythia6/*datasetME.root
 hadd results/merged/Ztautau_Hi_me.root results/Spring11/DYToTauTau_M-20_TuneZ2_7TeV-pythia6-tauola/*datasetME.root
@@ -88,5 +80,5 @@ hadd results/merged/WW_me.root results/Spring11/WWTo2L2Nu_TuneZ2_7TeV-pythia6/*d
 hadd results/merged/ggWW_me.root results/Spring11/GluGluToWWTo4L_TuneZ2_7TeV-gg2ww-pythia6/*datasetME.root
 hadd results/merged/WZ_me.root results/Spring11/WZTo3LNu_TuneZ2_7TeV-pythia6/*datasetME.root
 hadd results/merged/ZZ_me.root results/Spring11/ZZtoAnything_TuneZ2_7TeV-pythia6-tauola/*datasetME.root
-#hadd results/merged/Wgamma_me.root results/Spring11/Wgamma/*datasetME.root
+hadd results/merged/Wgamma_me.root results/Spring11/PhotonVJets_7TeV-madgraph/*datasetME.root
 
