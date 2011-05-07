254c4b21eb460428eb6a673059bc090baf38ca50
emanuele <>
2011-05-07 22:23:54 +0000

    submitMassDependentAnalysis.pl CVS: ---------------------------------------------------------------------- add sleep for too many accesses to castor

--- submitMassDependentAnalysis.pl
+++ submitMassDependentAnalysis.pl
@@ -8,7 +8,6 @@ if($opt_p) {$prefix = $opt_p;}
 else { die "usage: ./submitMassDependentAnalysis.pl -p <prefix>";}
 
 @masses = (120,130,140,150,160,170,180,190,200,210,220,230,250,300,350,400,450,500,550,600);
-#@masses = (120,130);
 
 for($i=0; $i<($#masses+1); $i++) {
     $mass = $masses[$i];
@@ -22,22 +21,6 @@ for($i=0; $i<($#masses+1); $i++) {
     print "submitting signals...\n";
     $higgsList = "GluGluToHToWWTo2L2Nu_M-".$mass."_7TeV-powheg-pythia6";
     system("python cmst3_submit_manyfilesperjob.py Spring11_V2 $higgsList 5 HiggsApp 8nh $fullprefix 1");
-
-#    $higgsList = "GluGluToHToWWToLNuTauNu_M-".$mass."_7TeV-powheg-pythia6";
-#    system("python cmst3_submit_manyfilesperjob.py Spring11_V2 $higgsList 5 HiggsApp 8nh $fullprefix 1");
-    
-#    $higgsList = "GluGluToHToWWToTauNuQQ_M-".$mass."_7TeV-powheg-pythia6";
-#    system("python cmst3_submit_manyfilesperjob.py Spring11_V2 $higgsList 5 HiggsApp 8nh $fullprefix 1");
-    
-#    $higgsList = "VBF_HToWWTo2L2Nu_M-".$mass."_7TeV-powheg-pythia6";
-#    system("python cmst3_submit_manyfilesperjob.py Spring11_V2 $higgsList 5 HiggsApp 8nh $fullprefix 1");
-
-#    $higgsList = "VBF_HToWWToLNuTauNu_M-".$mass."_7TeV-powheg-pythia6";
-#    system("python cmst3_submit_manyfilesperjob.py Spring11_V2 $higgsList 5 HiggsApp 8nh $fullprefix 1");
-
-#    $higgsList =  "VBF_HToWWToTauNuQQ_M-".$mass."_7TeV-powheg-pythia6";
-#    system("python cmst3_submit_manyfilesperjob.py Spring11_V2 $higgsList 5 HiggsApp 8nh $fullprefix 1");
-
     print "done with signals.\n";
 
     print  "submitting top...\n";
@@ -49,20 +32,25 @@ for($i=0; $i<($#masses+1); $i++) {
     
     print  "submitting V+jets...\n";
     system("python cmst3_submit_manyfilesperjob.py Spring11_V2 WJetsToLNu_TuneZ2_7TeV-madgraph-tauola 5  HiggsApp 8nh $fullprefix 1");
+
+    sleep 500;
+
     system("python cmst3_submit_manyfilesperjob.py Spring11_V2 DYJetsToLL_TuneD6T_M-10To50_7TeV-madgraph-tauola 5  HiggsApp 8nh $fullprefix 1");
     system("python cmst3_submit_manyfilesperjob.py Spring11_V2 DYJetsToLL_TuneD6T_M-50_7TeV-madgraph-tauola 5  HiggsApp 8nh $fullprefix 1");
 
 #     system("python cmst3_submit_manyfilesperjob.py Spring11_V2 WToENu_TuneZ2_7TeV-pythia6 5 HiggsApp 8nh $fullprefix 1");
 #     system("python cmst3_submit_manyfilesperjob.py Spring11_V2 WToMuNu_TuneZ2_7TeV-pythia6 5 HiggsApp 8nh $fullprefix 1");
 #     system("python cmst3_submit_manyfilesperjob.py Spring11_V2 WToTauNu_TuneZ2_7TeV-pythia6-tauola 5 HiggsApp 8nh $fullprefix 1");
-#    system("python cmst3_submit_manyfilesperjob.py Spring11_V2 DYToEE_M-20_TuneZ2_7TeV-pythia6 5 HiggsApp 8nh $fullprefix 1");
-#    system("python cmst3_submit_manyfilesperjob.py Spring11_V2 DYToMuMu_M-20_TuneZ2_7TeV-pythia6 5 HiggsApp 8nh $fullprefix 1");
-#    system("python cmst3_submit_manyfilesperjob.py Spring11_V2 DYToTauTau_M-20_TuneZ2_7TeV-pythia6-tauola 5 HiggsApp 8nh $fullprefix 1");
-#    system("python cmst3_submit_manyfilesperjob.py Spring11_V2 DYToEE_M-10To20_TuneZ2_7TeV-pythia6 5 HiggsApp 8nh $fullprefix 1");
-#    system("python cmst3_submit_manyfilesperjob.py Spring11_V2 DYToMuMu_M-10To20_TuneZ2_7TeV-pythia6 5 HiggsApp 8nh $fullprefix 1");
-#    system("python cmst3_submit_manyfilesperjob.py Spring11_V2  5 HiggsApp 8nh $fullprefix 1");
+    system("python cmst3_submit_manyfilesperjob.py Spring11_V2 DYToEE_M-20_TuneZ2_7TeV-pythia6 5 HiggsApp 8nh $fullprefix 1");
+    system("python cmst3_submit_manyfilesperjob.py Spring11_V2 DYToMuMu_M-20_TuneZ2_7TeV-pythia6 5 HiggsApp 8nh $fullprefix 1");
+    system("python cmst3_submit_manyfilesperjob.py Spring11_V2 DYToTauTau_M-20_TuneZ2_7TeV-pythia6 5 HiggsApp 8nh $fullprefix 1");
+    system("python cmst3_submit_manyfilesperjob.py Spring11_V2 DYToEE_M-10To20_TuneZ2_7TeV-pythia6 5 HiggsApp 8nh $fullprefix 1");
+    system("python cmst3_submit_manyfilesperjob.py Spring11_V2 DYToMuMu_M-10To20_TuneZ2_7TeV-pythia6 5 HiggsApp 8nh $fullprefix 1");
+    system("python cmst3_submit_manyfilesperjob.py Spring11_V2 DYToTauTau_M-10To20_TuneZ2_7TeV-pythia6 5 HiggsApp 8nh $fullprefix 1");
     print  "done with V+jets.\n";
     
+    sleep 500;
+
     print  "submitting dibosons...\n";
     system("python cmst3_submit_manyfilesperjob.py Spring11_V2 WWTo2L2Nu_TuneZ2_7TeV-pythia6 5 HiggsApp 8nh $fullprefix 1");
     system("python cmst3_submit_manyfilesperjob.py Spring11_V2 GluGluToWWTo4L_TuneZ2_7TeV-gg2ww-pythia6 5 HiggsApp 8nh $fullprefix 1");
@@ -73,5 +61,7 @@ for($i=0; $i<($#masses+1); $i++) {
     
     print "\nDONE WITH MASS $mass GeV\n";
     print "<--------------------------\n";
+    
+    sleep 500;
 
 }
