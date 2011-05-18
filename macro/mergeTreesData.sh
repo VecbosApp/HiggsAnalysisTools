1178a13d5a68ad093ca99f397b6650521f73ffda
emanuele <>
2011-05-18 21:22:27 +0000

    run on SingleMU + Double MU etc

--- macro/mergeTreesData.sh
+++ macro/mergeTreesData.sh
@@ -2,20 +2,16 @@
 # ./mergeTrees.sh expects results of the selection in subdirectories such as results/H120, results/bkg1, results/bkg2...
 # it creates a merged root file in the self-created results/merged/
 
-# usage: ./mergeTrees.sh dataset ("eg", "mu")
-
-dataset=$1
-
 mkdir -p results_data/merged
 
 echo "Now merging EE datasets..."
 hadd results_data/merged/dataset_ee.root results_data/Data7TeV/DoubleElectron/*datasetEE.root
 
 echo "Now merging MM datasets..."
-hadd results_data/merged/dataset_mm.root results_data/Data7TeV/DoubleMu/*datasetMM.root
+hadd results_data/merged/dataset_mm.root results_data/Data7TeV/DoubleMu/*datasetMM.root results_data/Data7TeV/SingleMu/*datasetMM.root
 
 echo "Now merging EM datasets..."
-hadd results_data/merged/dataset_em.root results_data/Data7TeV/MuEG/*datasetEM.root
+hadd results_data/merged/dataset_em.root results_data/Data7TeV/MuEG/*datasetEM.root  results_data/Data7TeV/SingleMu/*datasetEM.root
 
 echo "Now merging ME datasets..."
-hadd results_data/merged/dataset_me.root results_data/Data7TeV/MuEG/*datasetME.root
+hadd results_data/merged/dataset_me.root results_data/Data7TeV/MuEG/*datasetME.root results_data/Data7TeV/SingleMu/*datasetME.root
