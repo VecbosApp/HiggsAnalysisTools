6fea954c72e587f9d07366c63d85872b97b8cf23
crovelli <>
2011-05-19 21:24:03 +0000

    fix pure MC estimation

--- macro/estimateWjets.cc
+++ macro/estimateWjets.cc
@@ -11,36 +11,36 @@
 enum { ee=0, mm=1, em=2, me=3 };
 
 // arrays filled from counters
-float nEv_init[4];          // channel
+float nEv_init[4];          
 float nEv_endWW[4];
 float nEv_end0j[4];
 float nEv_end1j[4];
 
 // arrays filled with final results
-float numAtHiggs_0j[4];     // channel
-float errAtHiggs_0j[4];     // channel
-float numAtHiggs_1j[4];     // channel
-float errAtHiggs_1j[4];     // channel
+float numAtHiggs_0j[4];     
+float errAtHiggs_0j[4];     
+float numAtHiggs_1j[4];
+float errAtHiggs_1j[4];     
 
-float numAtHiggsMC_0j[4];     // channel
-float errAtHiggsMC_0j[4];     // channel
-float numAtHiggsMC_1j[4];     // channel
-float errAtHiggsMC_1j[4];     // channel
+float numAtHiggsMC_0j[4];   
+float errAtHiggsMC_0j[4];   
+float numAtHiggsMC_1j[4];   
+float errAtHiggsMC_1j[4];   
 
 void countEvents(int mass, const char* channel);
 float quadrSum(float x1, float x2, float x3=0, float x4=0, float x5=0, float x6=0, float x7=0, float x8=0);
 float yieldErrPoisson(float nEst1, float n1, float nEst2=0, float n2=0, float nEst3=0, float n3=0, float nEst4=0, float n4=0, float nEst5=0, float n5=0, float nEst6=0, float n6=0);
 
 void estimateWjets() {
-
+  
   // W+jets number of events at WW level measured from data  
   float numAtWW[4];
   float errAtWW[4];
-
-  numAtWW[ee] = 3.8;
+  
+  numAtWW[ee] = (150. / 126. ) * 3.8;
   errAtWW[ee] = quadrSum(0.5, 0.6); // stat (+) syst
 
-  numAtWW[me] = 8.8;
+  numAtWW[me] = (150. / 126.) * 8.8;
   errAtWW[me] = quadrSum(0.8, 1.1); // stat (+) syst
   
   numAtWW[mm] = 6.33;
@@ -49,34 +49,25 @@ void estimateWjets() {
   numAtWW[em] = 6.75;
   errAtWW[em] = quadrSum(1.28, 3.38); // stat (+) syst
 
+
+
   // -------------------------------------------------------------------
   // for 1 mass only: taking MC files with kine variables to estimate the W+jets amount in the WW control region
   char file_mcTree[1000];
   sprintf(file_mcTree,"/cmsrm/pc24_2/emanuele/data/Higgs4.1.X/MC2011_LHLoose_V13/OptimMH120/datasets_trees/Wjets_ll.root");
   cout << "reading file " << file_mcTree << endl;
 
+
+  // -------------------------------------------------------------------
+  // this is the WWcontrol / WW all ratio - taken from MC
   TFile *fileWjets = TFile::Open(file_mcTree);
   TTree *treeWjets = (TTree*)fileWjets->Get("T1");
-    
+
+  // at WW
   TH1F *WjetsHEE = new TH1F("WjetsHEE","",50,0,180);
   TH1F *WjetsHMM = new TH1F("WjetsHMM","",50,0,180);
   TH1F *WjetsHEM = new TH1F("WjetsHEM","",50,0,180);
   TH1F *WjetsHME = new TH1F("WjetsHME","",50,0,180);
-
-  TH1F *WjetsHiggsSelHEE = new TH1F("WjetsHiggsSelHEE","",50,0,180);
-  TH1F *WjetsHiggsSelHMM = new TH1F("WjetsHiggsSelHMM","",50,0,180);
-  TH1F *WjetsHiggsSelHEM = new TH1F("WjetsHiggsSelHEM","",50,0,180);
-  TH1F *WjetsHiggsSelHME = new TH1F("WjetsHiggsSelHME","",50,0,180);
-
-  TH1F *WjetsHiggsSel1jHEE = new TH1F("WjetsHiggsSel1jHEE","",50,0,180);
-  TH1F *WjetsHiggsSel1jHMM = new TH1F("WjetsHiggsSel1jHMM","",50,0,180);
-  TH1F *WjetsHiggsSel1jHEM = new TH1F("WjetsHiggsSel1jHEM","",50,0,180);
-  TH1F *WjetsHiggsSel1jHME = new TH1F("WjetsHiggsSel1jHME","",50,0,180);
-
-  TH1F *WjetsHEEOut = new TH1F("WjetsHEEOut","",50,0,180);
-  TH1F *WjetsHMMOut = new TH1F("WjetsHMMOut","",50,0,180);
-  TH1F *WjetsHEMOut = new TH1F("WjetsHEMOut","",50,0,180);
-  TH1F *WjetsHMEOut = new TH1F("WjetsHMEOut","",50,0,180);
   
   treeWjets->Project("WjetsHEE","deltaPhi","(WWSel && finalstate==0)*weight*puweight");
   treeWjets->Project("WjetsHMM","deltaPhi","(WWSel && finalstate==1)*weight*puweight");
@@ -84,14 +75,25 @@ void estimateWjets() {
   treeWjets->Project("WjetsHME","deltaPhi","(WWSel && finalstate==3)*weight*puweight");
   
   float nWjetsTotalEE = WjetsHEE->Integral();
+  float nWjetsTotalEE_err = yieldErrPoisson(nWjetsTotalEE,WjetsHEE->GetEntries());
   float nWjetsTotalMM = WjetsHMM->Integral();
+  float nWjetsTotalMM_err = yieldErrPoisson(nWjetsTotalMM,WjetsHMM->GetEntries());
   float nWjetsTotalEM = WjetsHEM->Integral();
+  float nWjetsTotalEM_err = yieldErrPoisson(nWjetsTotalEM,WjetsHEM->GetEntries());
   float nWjetsTotalME = WjetsHME->Integral();
+  float nWjetsTotalME_err = yieldErrPoisson(nWjetsTotalME,WjetsHME->GetEntries());
+
+  // in the WW control region
+  TH1F *WjetsHEEOut = new TH1F("WjetsHEEOut","",50,0,180);
+  TH1F *WjetsHMMOut = new TH1F("WjetsHMMOut","",50,0,180);
+  TH1F *WjetsHEMOut = new TH1F("WjetsHEMOut","",50,0,180);
+  TH1F *WjetsHMEOut = new TH1F("WjetsHMEOut","",50,0,180);
 
   treeWjets->Project("WjetsHEEOut","deltaPhi","(eleInvMass>100 && WWSel && finalstate==0)*weight*puweight");
   treeWjets->Project("WjetsHMMOut","deltaPhi","(eleInvMass>100 && WWSel && finalstate==1)*weight*puweight");
   treeWjets->Project("WjetsHEMOut","deltaPhi","(eleInvMass>100 && WWSel && finalstate==2)*weight*puweight");
   treeWjets->Project("WjetsHMEOut","deltaPhi","(eleInvMass>100 && WWSel && finalstate==3)*weight*puweight");
+
   float nWjetsControlEE = WjetsHEEOut->Integral();
   float nWjetsControlEE_err = yieldErrPoisson(nWjetsControlEE,WjetsHEEOut->GetEntries());
   float nWjetsControlMM = WjetsHMMOut->Integral();
@@ -101,50 +103,30 @@ void estimateWjets() {
   float nWjetsControlME = WjetsHMEOut->Integral();
   float nWjetsControlME_err = yieldErrPoisson(nWjetsControlME,WjetsHMEOut->GetEntries());
 
+
+  // => W+jets number of events at WW level taken from MC
+  float numAtWWMC[4];
+  float errAtWWMC[4];
+  numAtWWMC[0] = nWjetsTotalEE;
+  numAtWWMC[1] = nWjetsTotalMM;
+  numAtWWMC[2] = nWjetsTotalEM;
+  numAtWWMC[3] = nWjetsTotalME;
+  
+  errAtWWMC[0] = nWjetsTotalEE_err;
+  errAtWWMC[1] = nWjetsTotalMM_err;
+  errAtWWMC[2] = nWjetsTotalEM_err;
+  errAtWWMC[3] = nWjetsTotalME_err;
+
   float nWjetsControl_Tot = nWjetsControlEE+nWjetsControlMM+nWjetsControlEM+nWjetsControlME;
   float nWjetsControl_Tot_err = quadrSum(nWjetsControlEE_err,nWjetsControlMM_err,nWjetsControlEM_err,nWjetsControlME_err);
-
-  treeWjets->Project("WjetsHiggsSelHEE","deltaPhi","(finalSelection && njets==0 && finalstate==0)*weight*puweight");
-  treeWjets->Project("WjetsHiggsSelHMM","deltaPhi","(finalSelection && njets==0 && finalstate==1)*weight*puweight");
-  treeWjets->Project("WjetsHiggsSelHEM","deltaPhi","(finalSelection && njets==0 && finalstate==2)*weight*puweight");
-  treeWjets->Project("WjetsHiggsSelHME","deltaPhi","(finalSelection && njets==0 && finalstate==3)*weight*puweight");
-
-  treeWjets->Project("WjetsHiggsSel1jHEE","deltaPhi","(finalSelection && njets==1 && finalstate==0)*weight*puweight");
-  treeWjets->Project("WjetsHiggsSel1jHMM","deltaPhi","(finalSelection && njets==1 && finalstate==1)*weight*puweight");
-  treeWjets->Project("WjetsHiggsSel1jHEM","deltaPhi","(finalSelection && njets==1 && finalstate==2)*weight*puweight");
-  treeWjets->Project("WjetsHiggsSel1jHME","deltaPhi","(finalSelection && njets==1 && finalstate==3)*weight*puweight");
   
-  float nWjetsHiggsSelTotal[4], nWjetsHiggsSelTotal_err[4];
+  // std::cout <<"from pure MC: in total = " << nWjetsTotalEE+nWjetsTotalMM+nWjetsTotalEM+nWjetsTotalME
+  //        << " events, in the control region = " << nWjetsControl_Tot << " +/- " << nWjetsControl_Tot_err << std::endl;
 
-  nWjetsHiggsSelTotal[ee] = WjetsHiggsSelHEE->Integral();
-  nWjetsHiggsSelTotal_err[ee] = yieldErrPoisson(nWjetsHiggsSelTotal[ee],WjetsHiggsSelHEE->GetEntries());
 
-  nWjetsHiggsSelTotal[mm] = WjetsHiggsSelHMM->Integral();
-  nWjetsHiggsSelTotal_err[mm] = yieldErrPoisson(nWjetsHiggsSelTotal[mm],WjetsHiggsSelHMM->GetEntries());
 
-  nWjetsHiggsSelTotal[em] = WjetsHiggsSelHEM->Integral();
-  nWjetsHiggsSelTotal_err[em] = yieldErrPoisson(nWjetsHiggsSelTotal[em],WjetsHiggsSelHEM->GetEntries());
-
-  nWjetsHiggsSelTotal[me] = WjetsHiggsSelHME->Integral();
-  nWjetsHiggsSelTotal_err[me] = yieldErrPoisson(nWjetsHiggsSelTotal[me],WjetsHiggsSelHME->GetEntries());
-
-  float nWjetsHiggsSel1jTotal[4], nWjetsHiggsSel1jTotal_err[4];
-
-  nWjetsHiggsSel1jTotal[ee] = WjetsHiggsSel1jHEE->Integral();
-  nWjetsHiggsSel1jTotal_err[ee] = yieldErrPoisson(nWjetsHiggsSel1jTotal[ee],WjetsHiggsSel1jHEE->GetEntries());
-
-  nWjetsHiggsSel1jTotal[mm] = WjetsHiggsSel1jHMM->Integral();
-  nWjetsHiggsSel1jTotal_err[mm] = yieldErrPoisson(nWjetsHiggsSel1jTotal[mm],WjetsHiggsSel1jHMM->GetEntries());
-
-  nWjetsHiggsSel1jTotal[em] = WjetsHiggsSel1jHEM->Integral();
-  nWjetsHiggsSel1jTotal_err[em] = yieldErrPoisson(nWjetsHiggsSel1jTotal[em],WjetsHiggsSel1jHEM->GetEntries());
-
-  nWjetsHiggsSel1jTotal[me] = WjetsHiggsSel1jHME->Integral();
-  nWjetsHiggsSel1jTotal_err[me] = yieldErrPoisson(nWjetsHiggsSel1jTotal[me],WjetsHiggsSel1jHME->GetEntries());
-  
-  std::cout <<"MC: in total = " << nWjetsTotalEE+nWjetsTotalMM+nWjetsTotalEM+nWjetsTotalME
-            << " events, in the control region = " << nWjetsControl_Tot << " +/- " << nWjetsControl_Tot_err << std::endl;
-
+  // -------------------------------------------------------------------  
+  // this is the WWcontrol estimate starting with the measure in WW and using the MC ratio to extrapolate
   float ratioInOut[4];
   float numWWcontrol[4], errWWcontrol[4];
 
@@ -164,6 +146,14 @@ void estimateWjets() {
   numWWcontrol[me] = numAtWW[me]*ratioInOut[me];
   errWWcontrol[me] = errAtWW[me]*ratioInOut[me];   
 
+  float nWjetsControlData_Tot = numWWcontrol[ee] + numWWcontrol[mm] + numWWcontrol[em] + numWWcontrol[me];
+  float nWjetsControlData_Tot_err = quadrSum(errWWcontrol[ee],errWWcontrol[mm],errWWcontrol[em],errWWcontrol[me]);
+  
+  std::cout << "W+jets TOTAL in WW control region: in MC = " << nWjetsControl_Tot << " +/- " << nWjetsControl_Tot_err
+            << "\t data = " << nWjetsControlData_Tot << " +/- " << nWjetsControlData_Tot_err << std::endl;
+  
+  std::cout << std::endl;
+
   std::cout << "summary WW control region:: -------------------------" << std::endl;
   for(int icha=0;icha<4;icha++) {
     char channelName[2];
@@ -173,15 +163,11 @@ void estimateWjets() {
     if(icha==me) sprintf(channelName,"ME");
     std::cout.precision(3);
     std::cout << channelName << ": data = " << numWWcontrol[icha] << " +/- " << errWWcontrol[icha] << std::endl;
-   
   }
 
-  float nWjetsControlData_Tot = numWWcontrol[ee] + numWWcontrol[mm] + numWWcontrol[em] + numWWcontrol[me];
-  float nWjetsControlData_Tot_err = quadrSum(errWWcontrol[ee],errWWcontrol[mm],errWWcontrol[em],errWWcontrol[me]);
 
-  std::cout << "W+jets TOTAL in WW control region in MC = " << nWjetsControl_Tot << " +/- " << nWjetsControl_Tot_err
-            << "\t data = " << nWjetsControlData_Tot << " +/- " << nWjetsControlData_Tot_err << std::endl;
- 
+
+
   std::cout << "----------------------------------------------------" << std::endl;
 
   ofstream textfile;
@@ -196,6 +182,13 @@ void estimateWjets() {
   tablefile2.open("WjetsYieldsData_ForTable_1j.txt", ios_base::app);
   tablefile2.precision(2);
 
+  ofstream tablefile3;
+  tablefile3.open("WjetsYieldsMC_ForTable_0j.txt", ios_base::app);
+  tablefile3.precision(2);
+
+  ofstream tablefile4;
+  tablefile4.open("WjetsYieldsMC_ForTable_1j.txt", ios_base::app);
+  tablefile4.precision(2);
 
   int masses[17] = {120,130,140,150,160,170,180,190,200,250,300,350,400,450,500,550,600};
   // -------------------------------------------------------------------
@@ -215,34 +208,34 @@ void estimateWjets() {
       numAtHiggs_0j[icha] = numAtWW[icha]*ratioEndWW_0j;
       errAtHiggs_0j[icha] = errAtWW[icha]*ratioEndWW_0j;   
 
-      numAtHiggsMC_0j[icha] = nWjetsHiggsSelTotal[icha];
-      errAtHiggsMC_0j[icha] = nWjetsHiggsSelTotal_err[icha];
-      
+      numAtHiggsMC_0j[icha] = numAtWWMC[icha]*ratioEndWW_0j;
+      errAtHiggsMC_0j[icha] = errAtWWMC[icha]*ratioEndWW_0j;   
+
       float ratioEndWW_1j = (nEv_endWW[icha]==0) ? 0. : nEv_end1j[icha]/nEv_endWW[icha];
       numAtHiggs_1j[icha] = numAtWW[icha]*ratioEndWW_1j;
       errAtHiggs_1j[icha] = errAtWW[icha]*ratioEndWW_1j;   
 
-      numAtHiggsMC_1j[icha] = nWjetsHiggsSel1jTotal[icha];
-      errAtHiggsMC_1j[icha] = nWjetsHiggsSel1jTotal_err[icha];
+      numAtHiggsMC_1j[icha] = numAtWWMC[icha]*ratioEndWW_1j;
+      errAtHiggsMC_1j[icha] = errAtWWMC[icha]*ratioEndWW_1j;   
 
       char channelName[2];
       if(icha==ee) sprintf(channelName,"EE");
       if(icha==mm) sprintf(channelName,"MM");
       if(icha==em) sprintf(channelName,"EM");
       if(icha==me) sprintf(channelName,"ME");
-      
+
       textfile << channelName << ": Higgs Mass = " << mass 
                << "\tdata 0 jet = " << numAtHiggs_0j[icha] << " +/- " << errAtHiggs_0j[icha] 
                << "\tdata 1 jet = " << numAtHiggs_1j[icha] << " +/- " << errAtHiggs_1j[icha] 
-               << "\tMC 0 jet = " << numAtHiggsMC_0j[icha] << " +/- " << errAtHiggsMC_0j[icha] 
-               << "\tMC 1 jet = " << numAtHiggsMC_1j[icha] << " +/- " << errAtHiggsMC_1j[icha]
+               << "\tMC 0 jet = " << (150./126.) * numAtHiggsMC_0j[icha] << " +/- " << errAtHiggsMC_0j[icha] 
+               << "\tMC 1 jet = " << (150./126.) * numAtHiggsMC_1j[icha] << " +/- " << errAtHiggsMC_1j[icha]
                << std::endl;
     }
+    
 
-
-    // summary table for limits                                                                                                             
+    // summary table for limits                 
     if (i==0) {
-      tablefile1 << "zero jets bin" << endl;
+      tablefile1 << "zero jets bin from data" << endl;
       tablefile1 << "\t mumu \t mue \t emu \t ee" << endl;
     }
     tablefile1 << mass
@@ -253,7 +246,7 @@ void estimateWjets() {
                << std::endl;
 
     if (i==0) {
-      tablefile2 << "one jet bin" << endl;
+      tablefile2 << "one jet bin from data" << endl;
       tablefile2 << "\t mumu \t mue \t emu \t ee" << endl;
     }
     tablefile2 << mass
@@ -261,6 +254,28 @@ void estimateWjets() {
                << " " << "\t" << numAtHiggs_1j[3] << " +/- " << errAtHiggs_1j[3]
                << " " << "\t" << numAtHiggs_1j[2] << " +/- " << errAtHiggs_1j[2]
                << " " << "\t" << numAtHiggs_1j[0] << " +/- " << errAtHiggs_1j[0]
+               << std::endl;
+
+    if (i==0) {
+      tablefile3 << "zero jets bin from MC" << endl;
+      tablefile3 << "\t mumu \t mue \t emu \t ee" << endl;
+    }
+    tablefile3 << mass
+               << " " << "\t" << (150./126.) * numAtHiggsMC_0j[1] << " +/- " << errAtHiggsMC_0j[1]
+               << " " << "\t" << (150./126.) * numAtHiggsMC_0j[3] << " +/- " << errAtHiggsMC_0j[3]
+               << " " << "\t" << (150./126.) * numAtHiggsMC_0j[2] << " +/- " << errAtHiggsMC_0j[2]
+               << " " << "\t" << (150./126.) * numAtHiggsMC_0j[0] << " +/- " << errAtHiggsMC_0j[0]
+               << std::endl;
+
+    if (i==0) {
+      tablefile4 << "one jet bin" << endl;
+      tablefile4 << "\t mumu \t mue \t emu \t ee" << endl;
+    }
+    tablefile4 << mass
+               << " " << "\t" << (150./126.) * numAtHiggsMC_1j[1] << " +/- " << errAtHiggsMC_1j[1]
+               << " " << "\t" << (150./126.) * numAtHiggsMC_1j[3] << " +/- " << errAtHiggsMC_1j[3]
+               << " " << "\t" << (150./126.) * numAtHiggsMC_1j[2] << " +/- " << errAtHiggsMC_1j[2]
+               << " " << "\t" << (150./126.) * numAtHiggsMC_1j[0] << " +/- " << errAtHiggsMC_1j[0]
                << std::endl;
 
     float numAtHiggs_0j_Tot = numAtHiggs_0j[ee] + numAtHiggs_0j[mm] + numAtHiggs_0j[em] + numAtHiggs_0j[me];
@@ -281,11 +296,10 @@ void estimateWjets() {
              << "\tMC 0 jet = " << numAtHiggsMC_0j_Tot << " +/- " << errAtHiggsMC_0j_Tot 
              << "\tMC 1 jet = " << numAtHiggsMC_1j_Tot << " +/- " << errAtHiggsMC_1j_Tot 
              << std::endl;
-
   }
-
+  
   std::cout << "Full W+jets yields in data in:  WjetsYieldsData.txt " << std::endl;
-
+  
 }
 
 float quadrSum(float x1, float x2, float x3, float x4, float x5, float x6, float x7, float x8) {
