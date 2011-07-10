ad4fefc3fd80feb8603932c5964675ddb8c9764b
emanuele <>
2011-07-10 04:42:22 +0000

    - add two more variables needed for bkg estimate

--- macro/addWeightsToTree.cc
+++ macro/addWeightsToTree.cc
@@ -2,6 +2,7 @@
 #include <TTree.h>
 #include <TBranch.h>
 #include <TMath.h>
+#include <TVector3.h>
 #include <iostream>
 
 using namespace std;
@@ -63,6 +64,7 @@ void addWeights(const char* filename, float weight, int finalstate) {
     Float_t         bTagTrackCount;
     Float_t         bTagImpPar;
     Float_t         bTagSecVertex;
+    Float_t         leadingJetBTagTrackCount;
     Float_t         pt[2];
     Float_t         eta[2];
     Float_t         deta[2];
@@ -70,6 +72,19 @@ void addWeights(const char* filename, float weight, int finalstate) {
     Float_t         hoe[2];
     Float_t         see[2];
     Int_t           matched[2];
+    Float_t         pxTkMet;
+    Float_t         pyTkMet;
+    Float_t         pzTkMet;
+    Float_t         pxLeadJet;
+    Float_t         pyLeadJet;
+    Float_t         pzLeadJet;
+    Float_t         pxL1;
+    Float_t         pyL1;
+    Float_t         pzL1;
+    Float_t         pxL2;
+    Float_t         pyL2;
+    Float_t         pzL2;
+    Int_t           nSoftMu;
 
     treeOrig->SetBranchAddress("run", &run);
     treeOrig->SetBranchAddress("lumi", &lumi);
@@ -104,6 +119,7 @@ void addWeights(const char* filename, float weight, int finalstate) {
     treeOrig->SetBranchAddress("bTagTrackCount", &bTagTrackCount);
     treeOrig->SetBranchAddress("bTagImpPar", &bTagImpPar);
     treeOrig->SetBranchAddress("bTagSecVertex", &bTagSecVertex);
+    treeOrig->SetBranchAddress("leadingJetBTagTrackCount", &leadingJetBTagTrackCount);
     treeOrig->SetBranchAddress("pt", pt);
     treeOrig->SetBranchAddress("eta", eta);
     treeOrig->SetBranchAddress("deta", deta);
@@ -111,6 +127,19 @@ void addWeights(const char* filename, float weight, int finalstate) {
     treeOrig->SetBranchAddress("hoe", hoe);
     treeOrig->SetBranchAddress("see", see);
     treeOrig->SetBranchAddress("matched", matched);
+    treeOrig->SetBranchAddress("pxTkMet", &pxTkMet);
+    treeOrig->SetBranchAddress("pyTkMet", &pyTkMet);
+    treeOrig->SetBranchAddress("pzTkMet", &pzTkMet);
+    treeOrig->SetBranchAddress("pxLeadJet", &pxLeadJet);
+    treeOrig->SetBranchAddress("pyLeadJet", &pyLeadJet);
+    treeOrig->SetBranchAddress("pzLeadJet", &pzLeadJet);
+    treeOrig->SetBranchAddress("pxL1", &pxL1);
+    treeOrig->SetBranchAddress("pyL1", &pyL1);
+    treeOrig->SetBranchAddress("pzL1", &pzL1);
+    treeOrig->SetBranchAddress("pxL2", &pxL2);
+    treeOrig->SetBranchAddress("pyL2", &pyL2);
+    treeOrig->SetBranchAddress("pzL2", &pzL2);
+    treeOrig->SetBranchAddress("nSoftMu", &nSoftMu);
 
     // 
     Float_t pt_1,   pt_2;
@@ -132,6 +161,14 @@ void addWeights(const char* filename, float weight, int finalstate) {
     Int_t         i_promptDecay;
     Int_t         i_WWSel;
     Int_t         i_WWSel1j;
+    float deltaPhi_LL;    
+    float deltaPhi_LL_MET;
+    float deltaPhi_LL_JET;
+    float deltaPhi_MET_JET;
+    float leadingJetPt;
+    float L1eta, L1phi;
+    float L2eta, L2phi;
+    float dileptonPt;
 
     // the selected final state: ee=0, mm=1, em=2
     treeNew->Branch("finalstate", &finalstate, "finalstate/I");
@@ -149,6 +186,7 @@ void addWeights(const char* filename, float weight, int finalstate) {
     treeNew->Branch("deltaR", &deltaR, "deltaR/F");
     treeNew->Branch("gammaStMRSt", &gammaStMRSt, "gammaStMRSt/F");
     treeNew->Branch("eleInvMass", &eleInvMass, "eleInvMass/F");
+    treeNew->Branch("transvMass", &transvMass, "transvMass/F");
     treeNew->Branch("maxPtEle", &maxPtEle, "maxPtEle/F");
     treeNew->Branch("minPtEle", &minPtEle, "minPtEle/F");
     treeNew->Branch("detaLeptons", &detaLeptons, "detaLeptons/F");
@@ -171,6 +209,7 @@ void addWeights(const char* filename, float weight, int finalstate) {
     treeNew->Branch("bTagTrackCount", &bTagTrackCount, "bTagTrackCount/F");
     treeNew->Branch("bTagImpPar", &bTagImpPar, "bTagImpPar/F");
     treeNew->Branch("bTagSecVertex", &bTagSecVertex, "bTagSecVertex/F");
+    treeNew->Branch("leadingJetBTagTrackCount", &leadingJetBTagTrackCount, "leadingJetBTagTrackCount/F");
     treeNew->Branch("pt1", &pt_1, "pt1/F");
     treeNew->Branch("eta1", &eta_1, "eta1/F");
     treeNew->Branch("deta1", &deta_1, "deta1/F");
@@ -186,6 +225,30 @@ void addWeights(const char* filename, float weight, int finalstate) {
     treeNew->Branch("see2", &see_2, "see2/F");
     treeNew->Branch("matched2", &matched_2, "matched2/I");
     treeNew->Branch("expCosDphi", &expCosDphi, "expCosDphi/F");
+    treeNew->Branch("step", step, "step/O");
+    treeNew->Branch("pxTkMet", &pxTkMet, "pxTkMet/F");
+    treeNew->Branch("pyTkMet", &pyTkMet, "pyTkMet/F");
+    treeNew->Branch("pzTkMet", &pzTkMet, "pzTkMet/F");
+    treeNew->Branch("pxLeadJet", &pxLeadJet, "pxLeadJet/F");
+    treeNew->Branch("pyLeadJet", &pyLeadJet, "pyLeadJet/F");
+    treeNew->Branch("pzLeadJet", &pzLeadJet, "pzLeadJet/F");
+    treeNew->Branch("pxL1", &pxL1, "pxL1/F");
+    treeNew->Branch("pyL1", &pyL1, "pyL1/F");
+    treeNew->Branch("pzL1", &pzL1, "pzL1/F");
+    treeNew->Branch("pxL2", &pxL2, "pxL2/F");
+    treeNew->Branch("pyL2", &pyL2, "pyL2/F");
+    treeNew->Branch("pzL2", &pzL2, "pzL2/F");
+    treeNew->Branch("deltaPhi_LL", &deltaPhi_LL, "deltaPhi_LL/F");
+    treeNew->Branch("deltaPhi_LL_MET", &deltaPhi_LL_MET, "deltaPhi_LL_MET/F");
+    treeNew->Branch("deltaPhi_LL_JET", &deltaPhi_LL_JET, "deltaPhi_LL_JET/F");
+    treeNew->Branch("deltaPhi_MET_JET", &deltaPhi_MET_JET, "deltaPhi_MET_JET/F");
+    treeNew->Branch("dileptonPt", &dileptonPt, "dileptonPt/F");
+    treeNew->Branch("leadingJetPt", &leadingJetPt, "leadingJetPt/F");
+    treeNew->Branch("L1eta", &L1eta, "L1eta/F");
+    treeNew->Branch("L1phi", &L1phi, "L1phi/F");
+    treeNew->Branch("L2eta", &L2eta, "L2eta/F");
+    treeNew->Branch("L2phi", &L2phi, "L2phi/F");
+    treeNew->Branch("nSoftMu", &nSoftMu, "nSoftMu/I");
 
     float jetcat = 0;
     treeNew->Branch("jetcat", &jetcat,  "jetcat/F");
@@ -238,6 +301,35 @@ void addWeights(const char* filename, float weight, int finalstate) {
       i_promptDecay = (promptDecay) ? 1 : 0;
       i_WWSel = (step[13]) ? 1 : 0;
       i_WWSel1j = (step[19] && njets==1) ? 1 : 0;
+
+      if (finalLeptons) {
+        TVector3 TV_L1( pxL1, pyL1, pzL1 );
+        TVector3 TV_L2( pxL2, pyL2, pzL2 );
+        TVector3 TV_L1p2 = TV_L1 + TV_L2;
+        TVector3 TV_met( pxTkMet, pyTkMet, pzTkMet );
+        TVector3 TV_jet( pxLeadJet, pyLeadJet, pzLeadJet );
+        deltaPhi_LL     = (180./3.14) * TV_L1.DeltaPhi(TV_L2);
+        deltaPhi_LL_MET = (180./3.14) * TV_met.DeltaPhi(TV_L1p2);
+        deltaPhi_LL_JET = (180./3.14) * TV_jet.DeltaPhi(TV_L1p2);
+        deltaPhi_MET_JET = (180./3.14) * TV_jet.DeltaPhi(TV_met);
+        leadingJetPt = sqrt(pxLeadJet*pxLeadJet + pyLeadJet*pyLeadJet);
+        dileptonPt = TV_L1p2.Pt();
+        L1eta = TV_L1.Eta();
+        L2eta = TV_L2.Eta();
+        L1phi = TV_L1.Phi();
+        L2phi = TV_L2.Phi();
+      } else { 
+        L1eta = 100.;
+        L2eta = 100.;
+        L1phi = 100.;
+        L2phi = 100.;
+        deltaPhi_LL = -9999.;
+        deltaPhi_LL_MET = -9999.;
+        deltaPhi_LL_JET = -9999.;
+        deltaPhi_MET_JET = -9999.;
+        leadingJetPt = -9999.;
+        dileptonPt = -9999.;
+      } 
 
       consecevent = (float)j;
       treeNew->Fill();
