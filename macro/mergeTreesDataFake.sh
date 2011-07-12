d0c0f4441347a5292a085b8f2089404ffc6edf61
emanuele <>
2011-07-12 19:38:05 +0000

    adding single Mu

---
+++ macro/mergeTreesDataFake.sh
@@ -0,0 +1,2 @@
+hadd results_data/merged/dataset_fake_ee.root results_fake_ee/DoubleElectron/DoubleElectron_*-datasetEE.root
+hadd results_data/merged/dataset_fake_me.root results_fake_me/MuEG/MuEG_*-datasetME.root results_fake_me/SingleMu/SingleMu_*-datasetME.root 
