484150c345a261fcc366b350263d325b7eb9583b
emanuele <>
2011-07-24 01:15:51 +0000

    all masses together

--- macro/mergeFinalStates.sh
+++ macro/mergeFinalStates.sh
@@ -2,14 +2,16 @@
 # ./mergeFinalStates.sh merges the trees for ee/emu/mm final states in one for a combined fit
 # the variable "finalstate in the tree will distinguish the three"
 
-Hmass=$1
+
 
 # usage: ./mergeFinalStates.sh Hmass
-
-hadd -f results/datasets_trees/H$Hmass\_ll.root results/datasets_trees/H$Hmass\_ee.root results/datasets_trees/H$Hmass\_mm.root results/datasets_trees/H$Hmass\_em.root results/datasets_trees/H$Hmass\_me.root
+for i in 120 130 140 150 160 170 180 190 200 250 300 350 400 450 500 550 600
+do
+  hadd -f results/datasets_trees/H$i\_ll.root results/datasets_trees/H$i\_ee.root results/datasets_trees/H$i\_mm.root results/datasets_trees/H$i\_em.root results/datasets_trees/H$i\_me.root
+done
 hadd -f results/datasets_trees/WW_ll.root results/datasets_trees/WW_ee.root results/datasets_trees/WW_mm.root results/datasets_trees/WW_em.root results/datasets_trees/WW_me.root
 hadd -f results/datasets_trees/top_ll.root results/datasets_trees/top_ee.root results/datasets_trees/top_mm.root results/datasets_trees/top_em.root results/datasets_trees/top_me.root
 hadd -f results/datasets_trees/Wjets_ll.root results/datasets_trees/Wjets_ee.root results/datasets_trees/Wjets_mm.root results/datasets_trees/Wjets_em.root results/datasets_trees/Wjets_me.root
 hadd -f results/datasets_trees/Zjets_ll.root results/datasets_trees/Zjets_ee.root results/datasets_trees/Zjets_mm.root results/datasets_trees/Zjets_em.root results/datasets_trees/Zjets_me.root
 hadd -f results/datasets_trees/others_ll.root results/datasets_trees/others_ee.root results/datasets_trees/others_mm.root results/datasets_trees/others_em.root results/datasets_trees/others_me.root
-hadd -f results_data/merged/dataset_ll.root results_data/merged/dataset_ee.root results_data/merged/dataset_mm.root results_data/merged/dataset_em.root results_data/merged/dataset_me.root
+hadd -f results_data/datasets_trees/dataset_ll.root results_data/datasets_trees/dataset_ee.root results_data/datasets_trees/dataset_mm.root results_data/datasets_trees/dataset_em.root results_data/datasets_trees/dataset_me.root
