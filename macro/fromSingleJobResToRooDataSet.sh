1b2c4920471ea112136eeb310a75cf9860247ed2
emanuele <>
2011-07-24 01:16:15 +0000

    do all masses together minor fixes

--- macro/fromSingleJobResToRooDataSet.sh
+++ macro/fromSingleJobResToRooDataSet.sh
@@ -3,6 +3,8 @@
 # it runs several scripts that bring from the results of the selection stored in the directory "results/"
 # to the merged RooDataSets of the species used for the fit
 
+lumi=$1
+
 echo "MERGING TREES STEP..."
 chmod u+x ./mergeTrees.sh
 ./mergeTrees.sh
@@ -10,7 +12,7 @@ echo "MERGING TREES STEP DONE."
 
 echo "WEIGHTING TREES STEP..."
 chmod u+x ./weightTrees.sh
-./weightTrees.sh 35
+./weightTrees.sh $lumi $lumi $lumi
 echo "MERGING TREES STEP DONE."
 
 echo "MERGING WEIGHTED TREES ACCORDING FIT SPECIES DEFINITION..."
@@ -18,7 +20,12 @@ chmod u+x ./mergeMultiSamples.sh
 ./mergeMultiSamples.sh
 echo "MERGING WEIGHTED TREES ACCORDING FIT SPECIES DEFINITION DONE."
 
-echo "CREATING FIT ROODATASETS STEP..."
-chmod u+x ./createFitDatasets.sh
-./createFitDatasets.sh
-echo "CREATING FIT ROODATASETS DONE."
+echo "MERGING FINAL STATES..."
+chmod u+x ./mergeFinalStates.sh
+./mergeFinalStates.sh
+echo "MERGING FINAL STATES DONE."
+
+#echo "CREATING FIT ROODATASETS STEP..."
+#chmod u+x ./createFitDatasets.sh
+#./createFitDatasets.sh $Hmass
+#echo "CREATING FIT ROODATASETS DONE."
