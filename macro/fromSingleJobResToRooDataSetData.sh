2a16ebc150fcc3c2223ae3d6c802818709cfb166
emanuele <>
2011-08-09 08:20:38 +0000

    do not merge MC

--- macro/fromSingleJobResToRooDataSetData.sh
+++ macro/fromSingleJobResToRooDataSetData.sh
@@ -4,25 +4,21 @@
 echo "===> STARTING... <==="
 
 echo "MERGING TREES STEP..."
-chmod u+x ./mergeTreesData.sh
 ./mergeTreesData.sh
 echo "MERGING TREES STEP DONE."
 
 echo "WEIGHTING TREES STEP..."
-chmod u+x ./weightTreesData.sh
 ./weightTreesData.sh
 echo "MERGING TREES STEP DONE."
 
 echo "MERGING MULTIPLE DATASETS IN THE SAME FINAL STATE..."
-chmod u+x ./mergeMultiSamplesData.sh
 ./mergeMultiSamplesData.sh
 ./mergeMultiSamplesDataSkim.sh
 echo "MERGING MULTIPLE DATASETS IN THE SAME FINAL STATE DONE."
 
 echo "MERGING FINAL STATES..."
-chmod u+x ./mergeFinalStates.sh
-./mergeFinalStates.sh
-./mergeFinalStatesSkim.sh
+./mergeFinalStatesData.sh
+./mergeFinalStatesSkimData.sh
 echo "MERGING FINAL STATES DONE."
 
 #echo "CREATING ROODATASETS FOR THE FIT..."
