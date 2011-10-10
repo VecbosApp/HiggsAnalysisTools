52d8307ea43512f9c9d00615cae210924d36dea4
emanuele <>
2011-10-10 14:39:45 +0000

    - change the mass for the signal so it takes the correct PU - comment some not  needed samples

--- cmst3_submit_manyfilesperjob.py
+++ cmst3_submit_manyfilesperjob.py
@@ -20,17 +20,17 @@ application = sys.argv[4]
 prefix = sys.argv[6]
 isMC = int(sys.argv[7])
 if isMC != 0:
-    inputlist = "cmst3_41X/MC/"+process+"/"+dataset+".list"
+    inputlist = "cmst3_42X/MC/"+process+"/"+dataset+".list"
 else:
-    inputlist = "cmst3_41X/"+process+"/"+dataset+".list"
+    inputlist = "cmst3_42X/"+process+"/"+dataset+".list"
 # to write on the cmst3 cluster disks
 ################################################
 #castordir = "/castor/cern.ch/user/m/mpierini/CMST3/Vecbos/output/"
 #outputmain = castordir+output
 # to write on local disks
 ################################################
-castordir = "/castor/cern.ch/user/e/emanuele/Higgs4.1.X/"
-diskoutputdir = "/cmsrm/pc23_2/emanuele/data/Higgs4.1.X/"
+castordir = "/castor/cern.ch/user/e/emanuele/Higgs4.2.X/"
+diskoutputdir = "/cmsrm/pc23_2/emanuele/data/Higgs4.2.X/"
 outputmain = castordir+"/"+prefix+"/"+process+"/"+output
 diskoutputmain = diskoutputdir+"/"+prefix+"/"+process+"/"+output
 # prepare job to write on the cmst3 cluster disks
@@ -40,12 +40,13 @@ os.system("mkdir -p "+prefix+"/"+process+"/"+output+"/log/")
 os.system("mkdir -p "+prefix+"/"+process+"/"+output+"/input/")
 os.system("mkdir -p "+prefix+"/"+process+"/"+output+"/src/")
 outputroot = outputmain+"/root/"
-if castordir != "none": 
-    os.system("rfmkdir -p "+outputmain)
-    os.system("rfmkdir -p "+outputroot)
-    os.system("rfchmod 777 "+outputmain)
-    os.system("rfchmod 777 "+outputroot)
-else: os.system("mkdir -p "+outputroot)
+# if castordir != "none": 
+#     os.system("rfmkdir -p "+outputmain)
+#     os.system("rfmkdir -p "+outputroot)
+#     os.system("rfchmod 777 "+outputmain)
+#     os.system("rfchmod 777 "+outputroot)
+# else: os.system("mkdir -p "+outputroot)
+#os.system("mkdir -p "+outputroot)
 
 if diskoutputdir != "none": 
     os.system("ssh -o BatchMode=yes -o StrictHostKeyChecking=no pccmsrm23 mkdir -p "+diskoutputmain)
@@ -77,23 +78,28 @@ while (len(inputfiles) > 0):
     outputfile = open(outputname,'w')
     outputfile.write('#!/bin/bash\n')
     outputfile.write('export STAGE_HOST=castorcms\n')
-    outputfile.write('export STAGE_SVCCLASS=cmst3\n')
+    # outputfile.write('export STAGE_SVCCLASS=cmst3\n')
     #    outputfile.write('cd '+pwd)
     outputfile.write('cp '+pwd+'/pdfs_MC.root $WORKDIR\n')
     outputfile.write('cp -r '+pwd+"/"+prefix+'/config $WORKDIR\n')
+    outputfile.write('cp -r '+pwd+"/"+'/data $WORKDIR\n')
     outputfile.write('export SCRAM_ARCH=slc5_amd64_gcc434\n')
-    outputfile.write('cd /afs/cern.ch/user/e/emanuele/scratch0/higgs/CMSSW_4_1_3/\n')
+    outputfile.write('cd /afs/cern.ch/user/e/emanuele/scratch0/higgs/CMSSW_4_2_6/\n')
     outputfile.write('eval `scramv1 runtime -sh`\n')
     outputfile.write('cd $WORKDIR\n')
     outputfile.write(pwd+'/'+application+' '+inputfilename+" "+output+"_"+str(ijob)+" "+str(isMC)+" "+dataset+"\n")
 
 #    if castordir != "none": outputfile.write('./VecbosApp '+inputfilename+" "+" rfio://"+outputroot+output+"_"+str(ijob)+".root\n")
 #    else:  
-    outputfile.write('ls *.root | grep -v Z_calibFall08 | xargs -i rfcp {} '+outputroot+'\n')
+#    outputfile.write('ls *.root | grep -v Z_calibFall08 | xargs -i rfcp {} '+outputroot+'\n')
     outputfile.write('ls *.root | grep -v Z_calibFall08 | xargs -i scp -o BatchMode=yes -o StrictHostKeyChecking=no {} pccmsrm23:'+diskoutputmain+'/{}\n') 
     outputfile.close
     os.system("echo bsub -q "+queue+" -o "+prefix+"/"+output+"/log/"+output+"_"+str(ijob)+".log source "+pwd+"/"+outputname)
     os.system("bsub -q "+queue+" -o "+prefix+"/"+process+"/"+output+"/log/"+output+"_"+str(ijob)+".log source "+pwd+"/"+outputname+" -copyInput="+process+"_"+str(ijob))
     ijob = ijob+1
-    time.sleep(2)
+    # and now cope with the max number of accesses (3000, keep 2500 for the safe side)
+    totfiles = nfileperjob*ijob
+    if(totfiles % 2500 == 0):
+        time.sleep(500);
+    #time.sleep(1)
     continue
