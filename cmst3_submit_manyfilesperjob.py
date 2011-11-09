e5c953334663f38cabc2d107d4c073f1f649202a
emanuele <>
2011-11-09 23:11:29 +0000

    - change the release where doing cmsenv - change the target pc

--- cmst3_submit_manyfilesperjob.py
+++ cmst3_submit_manyfilesperjob.py
@@ -30,7 +30,7 @@ else:
 # to write on local disks
 ################################################
 castordir = "/castor/cern.ch/user/e/emanuele/Higgs4.2.X/"
-diskoutputdir = "/cmsrm/pc23_2/emanuele/data/Higgs4.2.X/"
+diskoutputdir = "/cmsrm/pc24_2/emanuele/data/Higgs4.2.X/"
 outputmain = castordir+"/"+prefix+"/"+process+"/"+output
 diskoutputmain = diskoutputdir+"/"+prefix+"/"+process+"/"+output
 # prepare job to write on the cmst3 cluster disks
@@ -49,7 +49,7 @@ outputroot = outputmain+"/root/"
 #os.system("mkdir -p "+outputroot)
 
 if diskoutputdir != "none": 
-    os.system("ssh -o BatchMode=yes -o StrictHostKeyChecking=no pccmsrm23 mkdir -p "+diskoutputmain)
+    os.system("ssh -o BatchMode=yes -o StrictHostKeyChecking=no pccmsrm24 mkdir -p "+diskoutputmain)
 
 #look for the current directory
 #######################################
@@ -85,7 +85,7 @@ while (len(inputfiles) > 0):
     outputfile.write('cp -r '+pwd+"/"+'/data $WORKDIR\n')
     outputfile.write('cp -r '+pwd+"/"+'/elebdtweights $WORKDIR\n')
     outputfile.write('export SCRAM_ARCH=slc5_amd64_gcc434\n')
-    outputfile.write('cd /afs/cern.ch/user/e/emanuele/scratch0/higgs/CMSSW_4_2_6/\n')
+    outputfile.write('cd /afs/cern.ch/user/e/emanuele/scratch0/higgs/CMSSW_4_2_8_patch7/\n')
     outputfile.write('eval `scramv1 runtime -sh`\n')
     outputfile.write('cd $WORKDIR\n')
     outputfile.write(pwd+'/'+application+' '+inputfilename+" "+output+"_"+str(ijob)+" "+str(isMC)+" "+dataset+"\n")
@@ -93,7 +93,7 @@ while (len(inputfiles) > 0):
 #    if castordir != "none": outputfile.write('./VecbosApp '+inputfilename+" "+" rfio://"+outputroot+output+"_"+str(ijob)+".root\n")
 #    else:  
 #    outputfile.write('ls *.root | grep -v Z_calibFall08 | xargs -i rfcp {} '+outputroot+'\n')
-    outputfile.write('ls *.root | grep -v Z_calibFall08 | xargs -i scp -o BatchMode=yes -o StrictHostKeyChecking=no {} pccmsrm23:'+diskoutputmain+'/{}\n') 
+    outputfile.write('ls *.root | grep -v Z_calibFall08 | xargs -i scp -o BatchMode=yes -o StrictHostKeyChecking=no {} pccmsrm24:'+diskoutputmain+'/{}\n') 
     outputfile.close
     os.system("echo bsub -q "+queue+" -o "+prefix+"/"+output+"/log/"+output+"_"+str(ijob)+".log source "+pwd+"/"+outputname)
     os.system("bsub -q "+queue+" -o "+prefix+"/"+process+"/"+output+"/log/"+output+"_"+str(ijob)+".log source "+pwd+"/"+outputname+" -copyInput="+process+"_"+str(ijob))
