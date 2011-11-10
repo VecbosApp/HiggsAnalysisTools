40037d2843fc3eef3444085959a65ca78c3547ed
crovelli <>
2011-11-10 02:48:08 +0000

    me added

--- macro/mergeTreesDataFake.sh
+++ macro/mergeTreesDataFake.sh
@@ -3,6 +3,7 @@
 # it creates a merged root file in the self-created results/merged/
 
 mkdir -p results_fake_ee/merged
+mkdir -p results_fake_me/merged
 
 echo "Now merging EE datasets..."
 hadd results_fake_ee/merged/dataset_DoubleElectron_ee.root          results_fake_ee/DoubleElectron/*datasetEE.root
@@ -11,8 +12,13 @@ hadd results_fake_ee/merged/dataset_DoubleElectron_Run2011B_ee.root results_fake
 hadd results_fake_ee/merged/dataset_SingleElectron_ee.root          results_fake_ee/SingleElectron/*datasetEE.root
 hadd results_fake_ee/merged/dataset_SingleElectron_Run2011B_ee.root results_fake_ee/SingleElectron_Run2011B/*datasetEE.root
 
-#echo "Now merging ME datasets..."
-#hadd results_fake_ee/merged/dataset_MuEG_me.root results_fake_ee/MuEG/*datasetME.root 
-#hadd results_fake_ee/merged/dataset_SingleMu_me.root results_fake_ee/SingleMu/*datasetME.root 
-#hadd results_fake_ee/merged/dataset_SingleElectron_me.root results_fake_ee/SingleElectron/*datasetME.root
+echo "Now merging ME datasets..."
+hadd results_fake_me/merged/dataset_MuEG_me.root          results_fake_me/MuEG/*datasetME.root 
+hadd results_fake_me/merged/dataset_MuEG_Run2011B_me.root results_fake_me/MuEG_Run2011B/*datasetME.root 
+
+hadd results_fake_me/merged/dataset_SingleMu_me.root          results_fake_me/SingleMu/*datasetME.root 
+hadd results_fake_me/merged/dataset_SingleMu_Run2011B_me.root results_fake_me/SingleMu_Run2011B/*datasetME.root 
+
+hadd results_fake_me/merged/dataset_SingleElectron_me.root          results_fake_me/SingleElectron/*datasetME.root
+hadd results_fake_me/merged/dataset_SingleElectron_Run2011B_me.root results_fake_me/SingleElectron_Run2011B/*datasetME.root
 
--- macro/weightTreesFakeRateData.sh
+++ macro/weightTreesFakeRateData.sh
@@ -6,6 +6,7 @@
 # usage: ./weightTreesData.sh
 
 mkdir results_fake_ee/merged_skim
+mkdir results_fake_me/merged_skim
 
 echo "Adding weights..."
 root -l -b <<EOF
@@ -15,9 +16,19 @@ root -l -b <<EOF
 addFRWeights();
 
 addWeights("results_fake_ee/merged/dataset_DoubleElectron_ee.root",          1.0, 102, 1, 1); 
-addWeights("results_fake_ee/merged/dataset_DoubleElectron_Run2011B_ee.root", 1.0, 102, 1, 1);                                                                             
+addWeights("results_fake_ee/merged/dataset_DoubleElectron_Run2011B_ee.root", 1.0, 102, 1, 1);            
+                                                                 
 addWeights("results_fake_ee/merged/dataset_SingleElectron_ee.root",          1.0, 100, 1, 1);          
 addWeights("results_fake_ee/merged/dataset_SingleElectron_Run2011B_ee.root", 1.0, 100, 1, 1);          
+
+addWeights("results_fake_me/merged/dataset_MuEG_me.root",          1.0, 104, 3, 1); 
+addWeights("results_fake_me/merged/dataset_MuEG_Run2011B_me.root", 1.0, 104, 3, 1); 
+
+addWeights("results_fake_me/merged/dataset_SingleMu_me.root",           1.0, 101, 3, 1); 
+addWeights("results_fake_me/merged/dataset_SingleMu_Run2011B_me.root",  1.0, 101, 3, 1); 
+
+addWeights("results_fake_me/merged/dataset_SingleElectron_me.root", 1.0, 100, 3, 1);  
+addWeights("results_fake_me/merged/dataset_SingleElectron_me.root", 1.0, 100, 3, 1);  
 
 .q
 
