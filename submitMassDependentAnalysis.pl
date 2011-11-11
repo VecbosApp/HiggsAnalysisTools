ecd55437a01b1844bcea7122aecd72b08f04e7d5
emanuele <>
2011-11-11 18:05:35 +0000

    - change to Fall 11 - comment the samples which are not ready/needed

--- submitMassDependentAnalysis.pl
+++ submitMassDependentAnalysis.pl
@@ -18,100 +18,106 @@ for($i=0; $i<($#masses+1); $i++) {
 
     print "submitting signals...\n";
     $higgsList = "GluGluToHToWWTo2L2Nu_M-".$mass."_7TeV-powheg-pythia6";
-    system("python cmst3_submit_manyfilesperjob.py Summer11_V1 $higgsList 15 HiggsApp 8nh $prefix 1");
+    system("python cmst3_submit_manyfilesperjob.py Fall11_V1 $higgsList 10 HiggsApp 8nh $prefix 1");
     
     $higgsList = "GluGluToHToWWToLNuTauNu_M-".$mass."_7TeV-powheg-pythia6";
-    system("python cmst3_submit_manyfilesperjob.py Summer11_V1 $higgsList 15 HiggsApp 8nh $prefix 1");
+    system("python cmst3_submit_manyfilesperjob.py Fall11_V1 $higgsList 10 HiggsApp 8nh $prefix 1");
     
     $higgsList = "VBF_HToWWTo2L2Nu_M-".$mass."_7TeV-powheg-pythia6";
-    system("python cmst3_submit_manyfilesperjob.py Summer11_V1 $higgsList 15 HiggsApp 8nh $prefix 1");
+    system("python cmst3_submit_manyfilesperjob.py Fall11_V1 $higgsList 10 HiggsApp 8nh $prefix 1");
     
     $higgsList = "VBF_HToWWToLNuTauNu_M-".$mass."_7TeV-powheg-pythia6";
-    system("python cmst3_submit_manyfilesperjob.py Summer11_V1 $higgsList 15 HiggsApp 8nh $prefix 1");
+    system("python cmst3_submit_manyfilesperjob.py Fall11_V1 $higgsList 10 HiggsApp 8nh $prefix 1");
 
 }   
 
 print "done with signals. Sleeping 600s...\n";
-sleep 600;
+#sleep 600;
 
 open(MASSFILE,">config/higgs/higgsMass.txt");
 print MASSFILE "HiggsMass\t 160\n";
 
 print  "submitting top...\n";
-system("python cmst3_submit_manyfilesperjob.py Summer11_V1 T_TuneZ2_tW-channel-DR_7TeV-powheg-tauola 15 HiggsApp 8nh $prefix 1");
-system("python cmst3_submit_manyfilesperjob.py Summer11_V1 T_TuneZ2_tW-channel-DS_7TeV-powheg-tauola 15 HiggsApp 8nh $prefix 1");
-system("python cmst3_submit_manyfilesperjob.py Summer11_V1 Tbar_TuneZ2_tW-channel-DR_7TeV-powheg-tauola 15 HiggsApp 8nh $prefix 1");
-system("python cmst3_submit_manyfilesperjob.py Summer11_V1 Tbar_TuneZ2_tW-channel-DS_7TeV-powheg-tauola 15 HiggsApp 8nh $prefix 1");
-system("python cmst3_submit_manyfilesperjob.py Summer11_V1 T_TuneZ2_t-channel_7TeV-powheg-tauola 15 HiggsApp 8nh $prefix 1");
-system("python cmst3_submit_manyfilesperjob.py Summer11_V1 Tbar_TuneZ2_t-channel_7TeV-powheg-tauola 15 HiggsApp 8nh $prefix 1");
-system("python cmst3_submit_manyfilesperjob.py Summer11_V1 T_TuneZ2_s-channel_7TeV-powheg-tauola 15 HiggsApp 8nh $prefix 1");
-system("python cmst3_submit_manyfilesperjob.py Summer11_V1 Tbar_TuneZ2_s-channel_7TeV-powheg-tauola 15 HiggsApp 8nh $prefix 1");
-system("python cmst3_submit_manyfilesperjob.py Summer11_V1 TTTo2L2Nu2B_7TeV-powheg-pythia6 15 HiggsApp 8nh $prefix 1");
+system("python cmst3_submit_manyfilesperjob.py Fall11_V1 T_TuneZ2_tW-channel-DR_7TeV-powheg-tauola 5 HiggsApp 8nh $prefix 1");
+system("python cmst3_submit_manyfilesperjob.py Fall11_V1 T_TuneZ2_tW-channel-DS_7TeV-powheg-tauola 5 HiggsApp 8nh $prefix 1");
+system("python cmst3_submit_manyfilesperjob.py Fall11_V1 Tbar_TuneZ2_tW-channel-DR_7TeV-powheg-tauola 5 HiggsApp 8nh $prefix 1");
+system("python cmst3_submit_manyfilesperjob.py Fall11_V1 Tbar_TuneZ2_tW-channel-DS_7TeV-powheg-tauola 5 HiggsApp 8nh $prefix 1");
+system("python cmst3_submit_manyfilesperjob.py Fall11_V1 T_TuneZ2_t-channel_7TeV-powheg-tauola 5 HiggsApp 8nh $prefix 1");
+system("python cmst3_submit_manyfilesperjob.py Fall11_V1 Tbar_TuneZ2_t-channel_7TeV-powheg-tauola 5 HiggsApp 8nh $prefix 1");
+system("python cmst3_submit_manyfilesperjob.py Fall11_V1 T_TuneZ2_s-channel_7TeV-powheg-tauola 5 HiggsApp 8nh $prefix 1");
+system("python cmst3_submit_manyfilesperjob.py Fall11_V1 Tbar_TuneZ2_s-channel_7TeV-powheg-tauola 5 HiggsApp 8nh $prefix 1");
+system("python cmst3_submit_manyfilesperjob.py Fall11_V1 TTTo2L2Nu2B_7TeV-powheg-pythia6 5 HiggsApp 8nh $prefix 1");
 print  "done with top.\n";
-
-print  "submitting V+jets...\n";
-system("python cmst3_submit_manyfilesperjob.py Summer11_V1 WJetsToLNu_TuneZ2_7TeV-madgraph-tauola 15  HiggsApp 8nh $prefix 1");
 
 sleep 600;
 
-system("python cmst3_submit_manyfilesperjob.py Summer11_V1 DYToEE_M-20_CT10_TuneZ2_7TeV-powheg-pythia 15 HiggsApp 8nh $prefix 1");
-system("python cmst3_submit_manyfilesperjob.py Summer11_V1 DYToMuMu_M-20_CT10_TuneZ2_7TeV-powheg-pythia 15 HiggsApp 8nh $prefix 1");
-system("python cmst3_submit_manyfilesperjob.py Summer11_V1 DYToTauTau_M-20_CT10_TuneZ2_7TeV-powheg-pythia-tauola 15 HiggsApp 8nh $prefix 1");
-system("python cmst3_submit_manyfilesperjob.py Summer11_V1 DYToEE_M-10To20_CT10_TuneZ2_7TeV-powheg-pythia 15 HiggsApp 8nh $prefix 1");
-system("python cmst3_submit_manyfilesperjob.py Summer11_V1 DYToMuMu_M-10To20_CT10_TuneZ2_7TeV-powheg-pythia 15 HiggsApp 8nh $prefix 1");
-system("python cmst3_submit_manyfilesperjob.py Summer11_V1 DYToTauTau_M-10To20_TuneZ2_7TeV-pythia6-tauola 15 HiggsApp 8nh $prefix 1");
+print  "submitting W+jets...\n";
+system("python cmst3_submit_manyfilesperjob.py Fall11_V1 WJetsToLNu_TuneZ2_7TeV-madgraph-tauola 10  HiggsApp 8nh $prefix 1");
+
+sleep 600;
+print  "submitting DY high mass...\n";
+system("python cmst3_submit_manyfilesperjob.py Fall11_V1 DYToEE_M-20_CT10_TuneZ2_7TeV-powheg-pythia 10 HiggsApp 8nh $prefix 1");
+system("python cmst3_submit_manyfilesperjob.py Fall11_V1 DYToMuMu_M-20_CT10_TuneZ2_7TeV-powheg-pythia 10 HiggsApp 8nh $prefix 1");
+system("python cmst3_submit_manyfilesperjob.py Fall11_V1 DYToTauTau_M-20_CT10_TuneZ2_7TeV-powheg-pythia-tauola 10 HiggsApp 8nh $prefix 1");
+
+sleep 600;
+print  "submitting DY low mass...\n";
+system("python cmst3_submit_manyfilesperjob.py Fall11_V1 DYToEE_M-10To20_CT10_TuneZ2_7TeV-powheg-pythia 10 HiggsApp 8nh $prefix 1");
+system("python cmst3_submit_manyfilesperjob.py Fall11_V1 DYToMuMu_M-10To20_CT10_TuneZ2_7TeV-powheg-pythia 10 HiggsApp 8nh $prefix 1");
+system("python cmst3_submit_manyfilesperjob.py Fall11_V1 DYToTauTau_M-10To20_TuneZ2_7TeV-pythia6-tauola 10 HiggsApp 8nh $prefix 1");
 print  "done with V+jets.\n";
 
 sleep 600;
 
 print  "submitting dibosons...\n";
-system("python cmst3_submit_manyfilesperjob.py Summer11_V1 WWJetsTo2L2Nu_TuneZ2_7TeV-madgraph-tauola 15 HiggsApp 8nh $prefix 1");
-system("python cmst3_submit_manyfilesperjob.py Summer11_V1 GluGluToWWTo4L_TuneZ2_7TeV-gg2ww-pythia6 15 HiggsApp 8nh $prefix 1");
-system("python cmst3_submit_manyfilesperjob.py Summer11_V1 WZTo3LNu_TuneZ2_7TeV_pythia6_tauola 15 HiggsApp 8nh $prefix 1");
-system("python cmst3_submit_manyfilesperjob.py Summer11_V1 GVJets_7TeV-madgraph 15 HiggsApp 8nh $prefix 1");
-system("python cmst3_submit_manyfilesperjob.py Summer11_V1 ZZ_TuneZ2_7TeV_pythia6_tauola 15 HiggsApp 8nh $prefix 1");
+system("python cmst3_submit_manyfilesperjob.py Fall11_V1 WWJetsTo2L2Nu_TuneZ2_7TeV-madgraph-tauola 10 HiggsApp 8nh $prefix 1");
+system("python cmst3_submit_manyfilesperjob.py Fall11_V1 GluGluToWWTo4L_TuneZ2_7TeV-gg2ww-pythia6 10 HiggsApp 8nh $prefix 1");
+system("python cmst3_submit_manyfilesperjob.py Fall11_V1 GVJets_7TeV-madgraph 10 HiggsApp 8nh $prefix 1");
+system("python cmst3_submit_manyfilesperjob.py Fall11_V1 ZZTo2L2Nu_TuneZ2_7TeV_pythia6_tauola 5 HiggsApp 8nh $prefix 1");
+#system("python cmst3_submit_manyfilesperjob.py Fall11_V1 WZJetsTo3LNu_TuneZ2_7TeV-madgraph-tauola 5 HiggsApp 8nh $prefix 1");
 print  "done with dibosons.\n";
 
 sleep 600;
 
 print  "submitting W+gamma and Z+gamma...\n";
-system("python cmst3_submit_manyfilesperjob.py Summer11_V1 WGToENuG_TuneZ2_7TeV-madgraph 15 HiggsApp 8nh $prefix 1");
-system("python cmst3_submit_manyfilesperjob.py Summer11_V1 WGToMuNuG_TuneZ2_7TeV-madgraph 15 HiggsApp 8nh $prefix 1");
-system("python cmst3_submit_manyfilesperjob.py Summer11_V1 WGToTauNuG_TuneZ2_7TeV-madgraph-tauola 15 HiggsApp 8nh $prefix 1");
-system("python cmst3_submit_manyfilesperjob.py Summer11_V1 ZGToEEG_TuneZ2_7TeV-madgraph 15 HiggsApp 8nh $prefix 1");
-system("python cmst3_submit_manyfilesperjob.py Summer11_V1 ZGToMuMuG_TuneZ2_7TeV-madgraph 15 HiggsApp 8nh $prefix 1");
-system("python cmst3_submit_manyfilesperjob.py Summer11_V1 ZGToTauTauG_TuneZ2_7TeV-madgraph-tauola 15 HiggsApp 8nh $prefix 1");
-system("python cmst3_submit_manyfilesperjob.py Summer11_V1 ZGToNuNuG_TuneZ2_7TeV-madgraph 15 HiggsApp 8nh $prefix 1");
+system("python cmst3_submit_manyfilesperjob.py Fall11_V1 WGToENuG_TuneZ2_7TeV-madgraph 5 HiggsApp 8nh $prefix 1");
+system("python cmst3_submit_manyfilesperjob.py Fall11_V1 WGToMuNuG_TuneZ2_7TeV-madgraph 5 HiggsApp 8nh $prefix 1");
+#system("python cmst3_submit_manyfilesperjob.py Fall11_V1 WGToTauNuG_TuneZ2_7TeV-madgraph-tauola 5 HiggsApp 8nh $prefix 1");
+system("python cmst3_submit_manyfilesperjob.py Fall11_V1 ZGToEEG_TuneZ2_7TeV-madgraph 5 HiggsApp 8nh $prefix 1");
+system("python cmst3_submit_manyfilesperjob.py Fall11_V1 ZGToMuMuG_TuneZ2_7TeV-madgraph 5 HiggsApp 8nh $prefix 1");
+system("python cmst3_submit_manyfilesperjob.py Fall11_V1 ZGToTauTauG_TuneZ2_7TeV-madgraph-tauola 5 HiggsApp 8nh $prefix 1");
+#system("python cmst3_submit_manyfilesperjob.py Fall11_V1 ZGToNuNuG_TuneZ2_7TeV-madgraph 5 HiggsApp 8nh $prefix 1");
 
 sleep 600;
 
-print  "submitting systematics samples for top...\n";
-system("python cmst3_submit_manyfilesperjob.py Summer11_V1 TT_TuneZ2_7TeV-pythia6-tauola 15 HiggsApp 8nh $prefix 1");
-system("python cmst3_submit_manyfilesperjob.py Summer11_V1 TTJets_TuneZ2_7TeV-madgraph-tauola 15 HiggsApp 8nh $prefix 1");
-system("python cmst3_submit_manyfilesperjob.py Summer11_V1 TTjets_TuneZ2_scaleup_7TeV-madgraph-tauola 15 HiggsApp 8nh $prefix 1");
-system("python cmst3_submit_manyfilesperjob.py Summer11_V1 T_TuneZ2_scaledown_tW-channel-DR 15 HiggsApp 8nh $prefix 1");
-system("python cmst3_submit_manyfilesperjob.py Summer11_V1 T_TuneZ2_scaledown_tW-channel-DS 15 HiggsApp 8nh $prefix 1");
-system("python cmst3_submit_manyfilesperjob.py Summer11_V1 T_TuneZ2_scaleup_tW-channel-DR 15 HiggsApp 8nh $prefix 1");
-system("python cmst3_submit_manyfilesperjob.py Summer11_V1 T_TuneZ2_scaleup_tW-channel-DS 15 HiggsApp 8nh $prefix 1");
-system("python cmst3_submit_manyfilesperjob.py Summer11_V1 Tbar_TuneZ2_scaledown_tW-channel-DR 15 HiggsApp 8nh $prefix 1");
-system("python cmst3_submit_manyfilesperjob.py Summer11_V1 Tbar_TuneZ2_scaledown_tW-channel-DS 15 HiggsApp 8nh $prefix 1");
-system("python cmst3_submit_manyfilesperjob.py Summer11_V1 Tbar_TuneZ2_scaleup_tW-channel-DR 15 HiggsApp 8nh $prefix 1");
-system("python cmst3_submit_manyfilesperjob.py Summer11_V1 Tbar_TuneZ2_scaleup_tW-channel-DS 15 HiggsApp 8nh $prefix 1");
+#print  "submitting systematics samples for top...\n";
+#system("python cmst3_submit_manyfilesperjob.py Fall11_V1 TT_TuneZ2_7TeV-pythia6-tauola 5 HiggsApp 8nh $prefix 1");
+#system("python cmst3_submit_manyfilesperjob.py Fall11_V1 TTJets_TuneZ2_7TeV-madgraph-tauola 5 HiggsApp 8nh $prefix 1");
+#system("python cmst3_submit_manyfilesperjob.py Fall11_V1 TTjets_TuneZ2_scaleup_7TeV-madgraph-tauola 5 HiggsApp 8nh $prefix 1");
+#system("python cmst3_submit_manyfilesperjob.py Fall11_V1 T_TuneZ2_scaledown_tW-channel-DR 5 HiggsApp 8nh $prefix 1");
+#system("python cmst3_submit_manyfilesperjob.py Fall11_V1 T_TuneZ2_scaledown_tW-channel-DS 5 HiggsApp 8nh $prefix 1");
+#system("python cmst3_submit_manyfilesperjob.py Fall11_V1 T_TuneZ2_scaleup_tW-channel-DR 5 HiggsApp 8nh $prefix 1");
+#system("python cmst3_submit_manyfilesperjob.py Fall11_V1 T_TuneZ2_scaleup_tW-channel-DS 5 HiggsApp 8nh $prefix 1");
+#system("python cmst3_submit_manyfilesperjob.py Fall11_V1 Tbar_TuneZ2_scaledown_tW-channel-DR 5 HiggsApp 8nh $prefix 1");
+#system("python cmst3_submit_manyfilesperjob.py Fall11_V1 Tbar_TuneZ2_scaledown_tW-channel-DS 5 HiggsApp 8nh $prefix 1");
+#system("python cmst3_submit_manyfilesperjob.py Fall11_V1 Tbar_TuneZ2_scaleup_tW-channel-DR 5 HiggsApp 8nh $prefix 1");
+#system("python cmst3_submit_manyfilesperjob.py Fall11_V1 Tbar_TuneZ2_scaleup_tW-channel-DS 5 HiggsApp 8nh $prefix 1");
 
-sleep 600;
+#sleep 600;
 
-print  "submitting systematics samples for WW...\n";
-system("python cmst3_submit_manyfilesperjob.py Summer11_V1 WWTo2L2Nu_TuneZ2_7TeV_pythia6_tauola 15 HiggsApp 8nh $prefix 1");
-system("python cmst3_submit_manyfilesperjob.py Summer11_V1 WWTo2L2Nu_CT10_7TeV-mcatnlo 15 HiggsApp 8nh $prefix 1");
-system("python cmst3_submit_manyfilesperjob.py Summer11_V1 WWTo2L2Nu_scaledown_CT10_7TeV-mcatnlo 15 HiggsApp 8nh $prefix 1");
-system("python cmst3_submit_manyfilesperjob.py Summer11_V1 WWTo2L2Nu_scaleup_CT10_7TeV-mcatnlo 15 HiggsApp 8nh $prefix 1");
+print  "submitting systematics samples for VV...\n";
+system("python cmst3_submit_manyfilesperjob.py Fall11_V1 WWTo2L2Nu_TuneZ2_7TeV_pythia6_tauola 10 HiggsApp 8nh $prefix 1");
+system("python cmst3_submit_manyfilesperjob.py Fall11_V1 WWTo2L2Nu_CT10_7TeV-mcatnlo 10 HiggsApp 8nh $prefix 1");
+system("python cmst3_submit_manyfilesperjob.py Fall11_V1 WWTo2L2Nu_scaledown_CT10_7TeV-mcatnlo 10 HiggsApp 8nh $prefix 1");
+system("python cmst3_submit_manyfilesperjob.py Fall11_V1 WWTo2L2Nu_scaleup_CT10_7TeV-mcatnlo 10 HiggsApp 8nh $prefix 1");
+#system("python cmst3_submit_manyfilesperjob.py Fall11_V1 ZZ_TuneZ2_7TeV_pythia6_tauola 10 HiggsApp 8nh $prefix 1");
+system("python cmst3_submit_manyfilesperjob.py Fall11_V1 WZTo3LNu_TuneZ2_7TeV_pythia6_tauola 10 HiggsApp 8nh $prefix 1");
+#sleep 600;
 
-sleep 600;
-
-print  "submitting systematics samples for Wjets...\n";
-system("python cmst3_submit_manyfilesperjob.py Summer11_V1 WJetsToLNu_TuneZ2_scaledown_7TeV-madgraph-tauola 15 HiggsApp 8nh $prefix 1");
-system("python cmst3_submit_manyfilesperjob.py Summer11_V1 WJetsToLNu_TuneZ2_scaleup_7TeV-madgraph-tauola 15 HiggsApp 8nh $prefix 1");
-system("python cmst3_submit_manyfilesperjob.py Summer11_V1 WJetsToLNu_TuneZ2_matchingup_7TeV-madgraph-tauola 15 HiggsApp 8nh $prefix 1");
+#print  "submitting systematics samples for Wjets...\n";
+#system("python cmst3_submit_manyfilesperjob.py Fall11_V1 WJetsToLNu_TuneZ2_scaledown_7TeV-madgraph-tauola 10 HiggsApp 8nh $prefix 1");
+#system("python cmst3_submit_manyfilesperjob.py Fall11_V1 WJetsToLNu_TuneZ2_scaleup_7TeV-madgraph-tauola 10 HiggsApp 8nh $prefix 1");
+#system("python cmst3_submit_manyfilesperjob.py Fall11_V1 WJetsToLNu_TuneZ2_matchingup_7TeV-madgraph-tauola 10 HiggsApp 8nh $prefix 1");
 
 print "\nDONE WITH MASS $mass GeV\n";
 print "<--------------------------\n";
