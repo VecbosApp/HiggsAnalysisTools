e7cc6597c4eee94c2763e0561e063a460185e5c6
emanuele <>
2011-11-12 00:11:50 +0000

    remove the OF/SF

--- macro/mergeFinalStates.sh
+++ macro/mergeFinalStates.sh
@@ -8,26 +8,14 @@
 for i in 120 130 140 150 160 170 180 190 200 250 300 350 400 450 500 550 600
 do
   hadd results/datasets_trees/H$i\_ll.root results/datasets_trees/H$i\_ee.root results/datasets_trees/H$i\_mm.root results/datasets_trees/H$i\_em.root results/datasets_trees/H$i\_me.root
-  hadd results/datasets_trees/H$i\_sf.root results/datasets_trees/H$i\_ee.root results/datasets_trees/H$i\_mm.root
-  hadd results/datasets_trees/H$i\_of.root results/datasets_trees/H$i\_em.root results/datasets_trees/H$i\_me.root
 done
 hadd results/datasets_trees/WW_ll.root results/datasets_trees/WW_ee.root results/datasets_trees/WW_mm.root results/datasets_trees/WW_em.root results/datasets_trees/WW_me.root
-hadd results/datasets_trees/WW_sf.root results/datasets_trees/WW_ee.root results/datasets_trees/WW_mm.root
-hadd results/datasets_trees/WW_of.root results/datasets_trees/WW_em.root results/datasets_trees/WW_me.root
 
 hadd results/datasets_trees/top_ll.root results/datasets_trees/top_ee.root results/datasets_trees/top_mm.root results/datasets_trees/top_em.root results/datasets_trees/top_me.root
-hadd results/datasets_trees/top_sf.root results/datasets_trees/top_ee.root results/datasets_trees/top_mm.root
-hadd results/datasets_trees/top_of.root  results/datasets_trees/top_em.root results/datasets_trees/top_me.root
 
 hadd results/datasets_trees/Wjets_ll.root results/datasets_trees/Wjets_ee.root results/datasets_trees/Wjets_mm.root results/datasets_trees/Wjets_em.root results/datasets_trees/Wjets_me.root
-hadd results/datasets_trees/Wjets_sf.root results/datasets_trees/Wjets_ee.root results/datasets_trees/Wjets_mm.root 
-hadd results/datasets_trees/Wjets_of.root results/datasets_trees/Wjets_em.root results/datasets_trees/Wjets_me.root
 
 hadd results/datasets_trees/Zjets_ll.root results/datasets_trees/Zjets_ee.root results/datasets_trees/Zjets_mm.root results/datasets_trees/Zjets_em.root results/datasets_trees/Zjets_me.root
-hadd results/datasets_trees/Zjets_sf.root results/datasets_trees/Zjets_ee.root results/datasets_trees/Zjets_mm.root 
-hadd results/datasets_trees/Zjets_of.root results/datasets_trees/Zjets_em.root results/datasets_trees/Zjets_me.root
 
 hadd results/datasets_trees/others_ll.root results/datasets_trees/others_ee.root results/datasets_trees/others_mm.root results/datasets_trees/others_em.root results/datasets_trees/others_me.root
-hadd results/datasets_trees/others_sf.root results/datasets_trees/others_ee.root results/datasets_trees/others_mm.root 
-hadd results/datasets_trees/others_of.root results/datasets_trees/others_em.root results/datasets_trees/others_me.root 
 
