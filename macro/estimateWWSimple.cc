1d0ee059c5f2202d9eec67e2298efc786ce17384
emanuele <>
2011-11-13 01:17:47 +0000

    adding propagation to 110 and 120 GeV

--- macro/estimateWWSimple.cc
+++ macro/estimateWWSimple.cc
@@ -362,10 +362,10 @@ void estimateWW() {
   TH1F *WWMEHFin = new TH1F("WWMEHFin","",50,0,180); // all
     
 
-  int masses[17] = {120,130,140,150,160,170,180,190,200,250,300,350,400,450,500,550,600};
+  int masses[19] = {110,115,120,130,140,150,160,170,180,190,200,250,300,350,400,450,500,550,600};
   // -------------------------------------------------------------------
   // now considering all masses to estimate the number of events at the end of the HWW selection
-  for (int i=0; i<17; i++) {
+  for (int i=0; i<19; i++) {
     
     int mass = masses[i];
     TString higgsMassDependentCut = higgsCuts(mass,true);
