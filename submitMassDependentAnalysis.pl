a8e6e9bcc1956e99872a6214761bc3a467e16a22
emanuele <>
2012-06-24 08:40:08 +0000

    ad the exclusive sample

--- submitMassDependentAnalysis.pl
+++ submitMassDependentAnalysis.pl
@@ -42,6 +42,7 @@ if($year==2012) {
     system("python cmst3_submit_manyfilesperjob.py Summer12_V14_52X T_t-channel_TuneZ2star_8TeV-powheg-tauola 5 HiggsApp 8nh $prefix 1 2012");
     system("python cmst3_submit_manyfilesperjob.py Summer12_V14_52X Tbar_t-channel_TuneZ2star_8TeV-powheg-tauola 5 HiggsApp 8nh $prefix 1 2012");
     system("python cmst3_submit_manyfilesperjob.py Summer12_V14_52X TTJets_TuneZ2star_8TeV-madgraph-tauola 5 HiggsApp 8nh $prefix 1 2012");
+    system("python cmst3_submit_manyfilesperjob.py Summer12_V14_52X TTTo2L2Nu2B_8TeV-powheg-pythia6 5 HiggsApp 8nh $prefix 1 2012");
     print  "done with top.\n";
 
     sleep 600;
