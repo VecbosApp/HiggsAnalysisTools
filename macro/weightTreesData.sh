9b3c8294c0e6439d5a8fd2422a64bab29fd5fc32
emanuele <>
2012-08-29 12:57:34 +0000

    used compiled program

--- macro/weightTreesData.sh
+++ macro/weightTreesData.sh
@@ -8,34 +8,21 @@
 mkdir -p results_data/merged_skim
 
 echo "Adding weights to tight x tight..."
-root -l -b <<EOF
+./LatinosAnalyzer results_data/merged/dataset_DoubleElectron_ee.root 1.0 102 1 0
+./LatinosAnalyzer results_data/merged/dataset_SingleElectron_ee.root 1.0 100 1 0
 
-.L addWeightsToTree.cc+
+./LatinosAnalyzer results_data/merged/dataset_DoubleMu_mm.root 1.0 103 0 0
+./LatinosAnalyzer results_data/merged/dataset_SingleMu_mm.root 1.0 101 0 0
 
-addWeights("results_data/merged/dataset_DoubleElectron_ee.root", 1.0, 102, 1, 0);
-addWeights("results_data/merged/dataset_SingleElectron_ee.root", 1.0, 100, 1, 0);
+./LatinosAnalyzer results_data/merged/dataset_MuEG_em.root 1.0 104 2 0
+./LatinosAnalyzer results_data/merged/dataset_SingleMu_em.root 1.0 101 2 0
+./LatinosAnalyzer results_data/merged/dataset_SingleElectron_em.root 1.0 100 2 0
 
-addWeights("results_data/merged/dataset_DoubleMu_mm.root", 1.0, 103, 0, 0);
-addWeights("results_data/merged/dataset_SingleMu_mm.root", 1.0, 101, 0, 0);
+./LatinosAnalyzer results_data/merged/dataset_MuEG_me.root 1.0 104 3 0
+./LatinosAnalyzer results_data/merged/dataset_SingleMu_me.root 1.0 101 3 0
+./LatinosAnalyzer results_data/merged/dataset_SingleElectron_me.root 1.0 100 3 0
 
-addWeights("results_data/merged/dataset_MuEG_em.root", 1.0, 104, 2, 0);
-addWeights("results_data/merged/dataset_SingleMu_em.root", 1.0, 101, 2, 0);
-addWeights("results_data/merged/dataset_SingleElectron_em.root", 1.0, 100, 2, 0);
-
-addWeights("results_data/merged/dataset_MuEG_me.root", 1.0, 104, 3, 0);
-addWeights("results_data/merged/dataset_SingleMu_me.root", 1.0, 101, 3, 0);
-addWeights("results_data/merged/dataset_SingleElectron_me.root", 1.0, 100, 3, 0);
-
-.q
-
-EOF
-
-#echo "Adding weights to loose x loose..."
-#root -l -b <<EOF
-#.L addWeightsToTreeLooseLoose.C+
-#addWeightsToTreeLooseLoose looper
-#looper.Loop()
-#.q
-#EOF
+make LatinosLooseLooseAnalyzer
+./LatinosLooseLooseAnalyzer results_data/datasets_trees/dataset_looseloose.root
 
 echo "done weighting."
