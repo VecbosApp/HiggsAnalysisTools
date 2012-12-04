3bc6d4dcf2dade381cdc2a8bac259c15a27f38db
emanuele <>
2012-12-04 11:24:45 +0000

    several fixes

--- fitmacro/PdfSystematics.hh
+++ fitmacro/PdfSystematics.hh
@@ -13,26 +13,26 @@ public:
 
   string getFormulaSyst() {
     stringstream fss;
-    fss << "@0*(1+@1+@2+@3+@4+@5+@6+@7+@8+@9+@10+@11+@12";
-    if(_process.find("qqww")!=string::npos) fss << "+@13+@14+@15";
+    fss << "@0*(1+@1+@2+@3+@4+@5+@6+@7+@8+@9";
+    if(_process.find("qqww")!=string::npos) fss << "+@10+@11+@12";
     fss << ")";
     return fss.str();
   }
 
   RooArgList getParSystematics(string par, string chstr, string tevstr, float centralval) {
-    RooRealVar *WW_par = new RooRealVar((_process+"_"+chstr+tevstr+"_"+par).c_str(), "", 0., -10., 10.);
-    RooRealVar *WW_par_err_MC = new RooRealVar((_process+"_"+chstr+tevstr+par+"_err_MC").c_str(), "", 0., -10., 10.);
-    RooRealVar *WW_par_err_scaleup = new RooRealVar((_process+"_"+chstr+tevstr+par+"_err_scaleup").c_str(), "", 0., -10., 10.);
-    RooRealVar *WW_par_err_scaledn = new RooRealVar((_process+"_"+chstr+tevstr+par+"_err_scaledn").c_str(), "", 0., -10., 10.);
-    RooRealVar *WW_par_err_resmet = new RooRealVar((_process+"_"+chstr+tevstr+par+"_err_resmet").c_str(), "", 0., -10., 10.);
-    RooRealVar *WW_par_err_rese = new RooRealVar((_process+"_"+chstr+tevstr+par+"_err_rese").c_str(), "", 0., -10., 10.);
-    RooRealVar *WW_par_err_resmu = new RooRealVar((_process+"_"+chstr+tevstr+par+"_err_resmu").c_str(), "", 0., -10., 10.);
-    RooRealVar *WW_par_err_scaleupe = new RooRealVar((_process+"_"+chstr+tevstr+par+"_err_scaleupe").c_str(), "", 0., -10., 10.);
-    RooRealVar *WW_par_err_scaledne = new RooRealVar((_process+"_"+chstr+tevstr+par+"_err_scaledne").c_str(), "", 0., -10., 10.);
-    RooRealVar *WW_par_err_scaleupmu = new RooRealVar((_process+"_"+chstr+tevstr+par+"_err_scaleupmu").c_str(), "", 0., -10., 10.);
-    RooRealVar *WW_par_err_scalednmu = new RooRealVar((_process+"_"+chstr+tevstr+par+"_err_scalednmu").c_str(), "", 0., -10., 10.);
-    RooRealVar *WW_par_err_scaleupj = new RooRealVar((_process+"_"+chstr+tevstr+par+"_err_scaleupj").c_str(), "", 0., -10., 10.);
-    RooRealVar *WW_par_err_scalednj = new RooRealVar((_process+"_"+chstr+tevstr+par+"_err_scalednj").c_str(), "", 0., -10., 10.);
+    RooRealVar *WW_par = new RooRealVar((_process+"_"+chstr+tevstr+"_"+par+"_nominal").c_str(), "", 0., -10000., 10000.);
+    RooRealVar *WW_par_err_MC = new RooRealVar((_process+"_"+chstr+tevstr+"_"+par+"_err_MC").c_str(), "", 0., -10., 10.);
+    RooRealVar *WW_par_err_scaleup = new RooRealVar((_process+"_"+chstr+tevstr+"_"+par+"_err_scaleup").c_str(), "", 0., -10., 10.);
+    RooRealVar *WW_par_err_scaledn = new RooRealVar((_process+"_"+chstr+tevstr+"_"+par+"_err_scaledn").c_str(), "", 0., -10., 10.);
+    RooRealVar *WW_par_err_resmet = new RooRealVar((_process+"_"+chstr+tevstr+"_"+par+"_err_res-met").c_str(), "", 0., -10., 10.);
+    RooRealVar *WW_par_err_rese = new RooRealVar((_process+"_"+chstr+tevstr+"_"+par+"_err_res-e").c_str(), "", 0., -10., 10.);
+    RooRealVar *WW_par_err_resmu = new RooRealVar((_process+"_"+chstr+tevstr+"_"+par+"_err_res-mu").c_str(), "", 0., -10., 10.);
+    RooRealVar *WW_par_err_scaleupe = new RooRealVar((_process+"_"+chstr+tevstr+"_"+par+"_err_scaleup-e").c_str(), "", 0., -10., 10.);
+    RooRealVar *WW_par_err_scaledne = new RooRealVar((_process+"_"+chstr+tevstr+"_"+par+"_err_scaledn-e").c_str(), "", 0., -10., 10.);
+    RooRealVar *WW_par_err_scaleupmu = new RooRealVar((_process+"_"+chstr+tevstr+"_"+par+"_err_scaleup-mu").c_str(), "", 0., -10., 10.);
+    RooRealVar *WW_par_err_scalednmu = new RooRealVar((_process+"_"+chstr+tevstr+"_"+par+"_err_scaledn-mu").c_str(), "", 0., -10., 10.);
+    RooRealVar *WW_par_err_scaleupj = new RooRealVar((_process+"_"+chstr+tevstr+"_"+par+"_err_scaleup-j").c_str(), "", 0., -10., 10.);
+    RooRealVar *WW_par_err_scalednj = new RooRealVar((_process+"_"+chstr+tevstr+"_"+par+"_err_scaledn-j").c_str(), "", 0., -10., 10.);
 
     WW_par->setVal(centralval);
     WW_par->setConstant(kTRUE);
@@ -69,9 +69,9 @@ public:
   }
 
   RooArgList getParSystematics(string par, string chstr, string tevstr, float centralval) {
-    RooRealVar *WJets_par = new RooRealVar((_process+"_"+chstr+tevstr+"_"+par).c_str(), "", 0., -10., 10.);
-    RooRealVar *WJets_par_err_fakerateup = new RooRealVar((_process+"_"+chstr+tevstr+par+"_err_fakerateup").c_str(), "", 0., -10., 10.);
-    RooRealVar *WJets_par_err_fakeratedn = new RooRealVar((_process+"_"+chstr+tevstr+par+"_err_fakeratedn").c_str(), "", 0., -10., 10.);
+    RooRealVar *WJets_par = new RooRealVar((_process+"_"+chstr+tevstr+"_"+par+"_nominal").c_str(), "", 0., -10000., 100000.);
+    RooRealVar *WJets_par_err_fakerateup = new RooRealVar((_process+"_"+chstr+tevstr+"_"+par+"_err_fakerateup").c_str(), "", 0., -10., 10.);
+    RooRealVar *WJets_par_err_fakeratedn = new RooRealVar((_process+"_"+chstr+tevstr+"_"+par+"_err_fakeratedn").c_str(), "", 0., -10., 10.);
 
     WJets_par->setVal(centralval);
     WJets_par->setConstant(kTRUE);
