8f3128c539dc332dee8317050b8a1e201eb15cf0
emanuele <>
2012-12-13 14:34:54 +0000

    first commit

---
+++ fitmacro/scripts/combcards.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+# set -x
+
+source "$CMSSW_BASE/src/HWWAnalysis/ShapeAnalysis/test/env.sh"
+
+tail="_shape.txt"
+cwd=$PWD
+masses="110 115 120 130 140 150 160"
+lumi=12.1
+
+function usage() {
+	echo "$( basename $0) -p <prefix>"
+}
+
+prefix=
+while getopts "hp:" OPTION
+do
+    case $OPTION in
+        h)
+            usage
+            exit 1
+            ;;
+        p)
+            prefix=$OPTARG
+            ;;
+    esac
+done
+
+echo `shape-config.py`
+eval `shape-config.py $lumi`
+
+if [[ $prefix ]]; then
+    echo "Running in $prefix"
+    cd $prefix
+fi
+
+echo "Lumi $lumi"
+head="hww-${lumi}fb.mH"
+head2j="hww-${lumi}fb.mH"
+hline=$(printf '%.0s-' {1..80})
+combCmd='combineCards.py -S'
+echo $PWD
+for mass in $masses
+do
+
+	for jet in 0j 1j
+	do
+        echo $hline
+        echo " Combining cards $mass $jet"
+        echo $hline
+		if [ -e $head$mass".of_"$jet$tail ] && [ -e $head$mass".sf_"$jet$tail ] 
+		then
+			echo " ${mass}: of_${jet} sf_${jet} => comb_${jet}"
+			$combCmd "of_"$jet"="$head$mass".of_"$jet$tail  "sf_"$jet"="$head$mass".sf_"$jet$tail > $head$mass".comb_"$jet$tail 
+		fi
+	done
+
+	for fl in of sf
+	do
+		file0="${head}${mass}.${fl}_0j${tail}"
+		file1="${head}${mass}.${fl}_1j${tail}"
+		cmb="${head}${mass}.comb_${fl}${tail}"
+
+        echo $hline
+		echo " Combining cards $mass $fl ( ${file0} + ${file1} )"
+        echo $hline
+		if [ -e "${file0}" ] && [ -e "${file1}" ] 
+		then
+			echo " ${mass}: ${fl}_0j ${fl}_1j => comb_${fl}"
+			$combCmd "${fl}_0j=${file0}"  "${fl}_1j=${file1}"  > "${cmb}"
+		fi
+	done
+
+	if [ -e $head$mass".of_0j"$tail ] && [ -e $head$mass".sf_0j"$tail ] && [ -e $head$mass".of_1j"$tail ] && [ -e $head$mass".sf_1j"$tail ]
+	then
+		echo " $mass: sf_0j of_0j of_1j sf_1j => comb_0j1j "
+		$combCmd "of_0j="$head$mass".of_0j"$tail  "sf_0j="$head$mass".sf_0j"$tail "of_1j="$head$mass".of_1j"$tail  "sf_1j="$head$mass".sf_1j"$tail > $head$mass".comb_0j1j"$tail
+	fi
+
+	if [ -e $head$mass".comb_0j"$tail ] && [ -e $head$mass".comb_1j"$tail ] && [ -e $head$mass".of_2j"$tail ] && [ -e $head$mass".sf_2j"$tail ]
+	then
+		echo " $mass: comb_0j comb_1j of_2j sf_2j => comb_0j1j2j "
+		$combCmd HWW_0j=$head$mass".comb_0j"$tail HWW_1j=$head$mass".comb_1j"$tail HWW_2j_of=$head$mass".of_2j"$tail HWW_2j_sf=$head$mass".sf_2j"$tail > $head$mass.comb_0j1j2j$tail
+fi
+
+
+done
+echo "...Done"
+
+
---
+++ fitmacro/scripts/runLimits.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+
+import sys
+import os.path
+import optparse
+import fnmatch
+import subprocess
+import hwwlimits
+
+indir  = os.getcwd()+'/datacards/'
+outdir = os.getcwd()+'/limits/'
+def main():
+
+    usage = '''usage: %prog [opts] dctag'''
+    parser = optparse.OptionParser(usage)
+    parser.add_option('-s','--stepping',dest='stepping',help='Switch stepping on ', action='store_true', default=False)
+    parser.add_option('-1',dest='minuit1',help='Minuit ', action='store_true', default=False)
+    parser.add_option('-n',dest='dryrun',help='Dry run ', action='store_true', default=False)
+    parser.add_option('-o',dest='observed',help='Observed only', action='store_true', default=False)
+    parser.add_option('--prefix','-p',dest='prefix',help='prefix',default=None)
+    parser.add_option('-l', '--lumi'     , dest='lumi'        , help='Luminosity'                            , default=None   , type='float'   )
+
+    (opt, args) = parser.parse_args()
+    print 'Running with lumi',opt.lumi
+    
+    constraints = {
+        '*':'--rMin=0.01'
+    }
+
+    if len(args) != 1:
+        parser.error('One and only one datacard tag at the time')
+
+    tag = args[0]
+
+    if tag not in hwwlimits.dcnames['all']:
+        parser.error('Wrong tag: '+', '.join(sorted(hwwlimits.dcnames['all'])))
+
+    tmpl = 'hww-{lumi:.1f}fb.mH{mass}.{tag}_shape.txt'
+    masses = [115, 120, 125, 130, 135, 140, 145, 150, 155, 160, 170, 180]
+
+    if opt.prefix:
+        os.chdir(opt.prefix)
+        print 'Running in ',os.getcwd()
+
+    if not os.path.exists(indir):
+        print 'Error: datacard directory',indir,'not found'
+        sys.exit(-1)
+
+    allcards = [(mass,os.path.join(indir,tmpl.format(mass=mass, tag=tag, lumi=opt.lumi))) for mass in masses] 
+    for mass,card in allcards:
+        if not os.path.exists(card):
+            print 'Error: missing datacard: '+card
+            sys.exit(-1)
+
+    os.system('mkdir -p '+outdir)
+
+    tagname = 'HWW_'+tag+'_shape'
+    for mass,card in allcards:
+        exe  = 'combine -v 2 -M Asymptotic'
+        flags = ' -n %s -m %s %s'%(tagname,mass,card)
+        if opt.stepping:
+            flags = ' --minosAlgo stepping'+flags
+        if opt.minuit1:
+            flags = ' --minimizerAlgo Minuit'+flags
+        if opt.observed:
+            flags = ' --run observed'+flags
+        for c,flag in constraints.iteritems():
+            if fnmatch.fnmatch(str(mass),c):
+                flags = ' '+flag+flags
+
+        command = exe+flags
+
+        print '-'*50
+        print command
+        if opt.dryrun: continue
+        code = os.system(command)
+        move = 'mv higgsCombine%s.Asymptotic.mH%d.root %s' % (tagname,mass,outdir)
+        print move
+        os.system(move)
+        
+        if code: sys.exit(code)
+
+if __name__ == '__main__':
+    main()
+
