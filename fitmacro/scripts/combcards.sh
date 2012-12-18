dd7cac83c6b330b2b2662d1de85beae15cc1e08b
emanuele <>
2012-12-18 17:01:57 +0000

    add the possibility to combine the 2D datacards

--- fitmacro/scripts/combcards.sh
+++ fitmacro/scripts/combcards.sh
@@ -3,17 +3,19 @@
 
 source "$CMSSW_BASE/src/HWWAnalysis/ShapeAnalysis/test/env.sh"
 
-tail="_shape.txt"
+tailsf="_shape.txt"
+tailof="_shape.txt"
 cwd=$PWD
 masses="110 115 120 125 130 135 140 145 150 155 160 170 180"
 lumi=12.1
 
 function usage() {
-	echo "$( basename $0) -p <prefix>"
+	echo "$( basename $0) -p <prefix> -t (to use 2D OF)"
 }
 
 prefix=
-while getopts "hp:" OPTION
+suffix2D=
+while getopts "hp:t" OPTION
 do
     case $OPTION in
         h)
@@ -22,6 +24,9 @@ do
             ;;
         p)
             prefix=$OPTARG
+            ;;
+        t)
+            tailof="_shape_2D.txt"
             ;;
     esac
 done
@@ -48,18 +53,24 @@ do
         echo $hline
         echo " Combining cards $mass $jet"
         echo $hline
-		if [ -e $head$mass".of_"$jet$tail ] && [ -e $head$mass".sf_"$jet$tail ] 
+		if [ -e $head$mass".of_"$jet$tailof ] && [ -e $head$mass".sf_"$jet$tailsf ] 
 		then
 			echo " ${mass}: of_${jet} sf_${jet} => comb_${jet}"
-			$combCmd "of_"$jet"="$head$mass".of_"$jet$tail  "sf_"$jet"="$head$mass".sf_"$jet$tail > $head$mass".comb_"$jet$tail 
+			$combCmd "of_"$jet"="$head$mass".of_"$jet$tailof  "sf_"$jet"="$head$mass".sf_"$jet$tailsf > $head$mass".comb_"$jet$tailof 
 		fi
 	done
 
 	for fl in of sf
 	do
-		file0="${head}${mass}.${fl}_0j${tail}"
-		file1="${head}${mass}.${fl}_1j${tail}"
-		cmb="${head}${mass}.comb_${fl}${tail}"
+                tailfl=
+                if [[ $fl == "of" ]]; then
+                    tailfl=$tailof
+                else
+                    tailfl=$tailsf
+                fi
+		file0="${head}${mass}.${fl}_0j${tailfl}"
+		file1="${head}${mass}.${fl}_1j${tailfl}"
+		cmb="${head}${mass}.comb_${fl}${tailfl}"
 
         echo $hline
 		echo " Combining cards $mass $fl ( ${file0} + ${file1} )"
@@ -71,16 +82,16 @@ do
 		fi
 	done
 
-	if [ -e $head$mass".of_0j"$tail ] && [ -e $head$mass".sf_0j"$tail ] && [ -e $head$mass".of_1j"$tail ] && [ -e $head$mass".sf_1j"$tail ]
+	if [ -e $head$mass".of_0j"$tailof ] && [ -e $head$mass".sf_0j"$tailsf ] && [ -e $head$mass".of_1j"$tailof ] && [ -e $head$mass".sf_1j"$tailsf ]
 	then
 		echo " $mass: sf_0j of_0j of_1j sf_1j => comb_0j1j "
-		$combCmd "of_0j="$head$mass".of_0j"$tail  "sf_0j="$head$mass".sf_0j"$tail "of_1j="$head$mass".of_1j"$tail  "sf_1j="$head$mass".sf_1j"$tail > $head$mass".comb_0j1j"$tail
+		$combCmd "of_0j="$head$mass".of_0j"$tailof  "sf_0j="$head$mass".sf_0j"$tailsf "of_1j="$head$mass".of_1j"$tailof  "sf_1j="$head$mass".sf_1j"$tailsf > $head$mass".comb_0j1j"$tailof
 	fi
 
-	if [ -e $head$mass".comb_0j"$tail ] && [ -e $head$mass".comb_1j"$tail ] && [ -e $head$mass".of_2j"$tail ] && [ -e $head$mass".sf_2j"$tail ]
+	if [ -e $head$mass".comb_0j"$tailof ] && [ -e $head$mass".comb_1j"$tailof ] && [ -e $head$mass".of_2j"$tailsf ] && [ -e $head$mass".sf_2j"$tailsf ]
 	then
 		echo " $mass: comb_0j comb_1j of_2j sf_2j => comb_0j1j2j "
-		$combCmd HWW_0j=$head$mass".comb_0j"$tail HWW_1j=$head$mass".comb_1j"$tail HWW_2j_of=$head$mass".of_2j"$tail HWW_2j_sf=$head$mass".sf_2j"$tail > $head$mass.comb_0j1j2j$tail
+		$combCmd HWW_0j=$head$mass".comb_0j"$tailof HWW_1j=$head$mass".comb_1j"$tailof HWW_2j_of=$head$mass".of_2j"$tailsf HWW_2j_sf=$head$mass".sf_2j"$tailsf > $head$mass.comb_0j1j2j$tailof
 fi
 
 
