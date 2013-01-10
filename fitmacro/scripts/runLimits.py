962c95e3e35dd40e7d443b3b7dd5b5fd0502f52e
emanuele <>
2013-01-10 16:03:47 +0000

    implement the toys with ML fit to study the fit bisas

--- fitmacro/scripts/runLimits.py
+++ fitmacro/scripts/runLimits.py
@@ -31,7 +31,7 @@ def main():
     print 'Running with lumi',opt.lumi
     
     constraints = {
-        '*':'--rMin=0.01'
+        '*':'--rMin=-5.0 --rMax=8.0'
     }
 
     if len(args) != 1:
@@ -44,6 +44,7 @@ def main():
 
     tmpl = 'hww-{lumi:.1f}fb.mH{mass}.{tag}_shape'+opt.suffix+'.txt'
     masses = [115, 120, 125, 130, 135, 140, 145, 150, 155, 160, 170, 180]
+    if opt.mlfit: masses = [125]
 
     if opt.prefix:
         os.chdir(opt.prefix)
@@ -74,6 +75,8 @@ def main():
                 flags = ' -M ProfileLikelihood --significance '+flags
             else:
                 flags = ' -M ProfileLikelihood --significance --expectSignal=1 -t 100 -s -1 '+flags
+        elif opt.mlfit:
+            flags = ' -M MaxLikelihoodFit --saveNormalizations --toysFrequentist --noErrors --minos none -t 5 -s -1 '+flags
         else:
             flags = ' -M Asymptotic '+flags
         if not opt.significance:
@@ -85,32 +88,33 @@ def main():
 
         print '-'*50
         print command
-        if opt.dryrun: continue
         if not opt.queue: code = os.system(command)
         if opt.significance:
             move = 'mv higgsCombine%s.ProfileLikelihood.mH%d*.root %s' % (tagname,mass,outdirsignif)
         else:
             move = 'mv higgsCombine%s.Asymptotic.mH%d.root %s' % (tagname,mass,outdir)
-        if not opt.queue: os.system(move)
-        else:
-            os.system('mkdir -p '+srcdir)
-            os.system('mkdir -p '+logdir)
-            jobsperpoint = 1 if opt.observed else 50
-            for j in range(jobsperpoint):
-                f = open(srcdir+'run-m'+str(mass)+'-j'+str(j)+'.src', 'w')
-                f.write('cd ~/workspace/hww2l2nu/CMSSW_5_3_3/\n')
-                f.write('eval `scram ru -sh` \n')
-                f.write('cd - \n')
-                f.write('source "$CMSSW_BASE/src/HWWAnalysis/ShapeAnalysis/test/env.sh"'+'\n')
-                f.write(command+'\n')
-                f.write(move+'\n')
-                f.close()
-                bsub = 'bsub -q %s -J mh%s%s-j%s -o %s/mh%s-j%s.log source %s/run-m%s-j%s.src' % (opt.queue,opt.suffix,mass,j,logdir,mass,j,srcdir,mass,j)
-                print '   job # '+str(j)
-                os.system(bsub)
+        os.system('mkdir -p '+srcdir)
+        os.system('mkdir -p '+logdir)
+        jobsperpoint = 1 if opt.observed else (1000 if opt.mlfit else 50)
+        for j in range(jobsperpoint):
+            f = open(srcdir+'run-m'+str(mass)+'-j'+str(j)+'.src', 'w')
+            f.write('cd ~/workspace/hww2l2nu/CMSSW_5_3_3/\n')
+            f.write('eval `scram ru -sh` \n')
+            f.write('cd - \n')
+            f.write('source "$CMSSW_BASE/src/HWWAnalysis/ShapeAnalysis/test/env.sh"'+'\n')
+            f.write(command+'\n')
+            if opt.mlfit:
+                move = 'mv higgsCombine%s.MaxLikelihoodFit.mH%d*.root %s ; mv mlfit%s.root %s/mlfit.mh%d.job%d.root' % (tagname,mass,outdir,tagname,outdir,mass,j)
+            f.write(move+'\n')
+            f.close()
+            bsub = 'bsub -q %s -J mh%s%s-j%s -o %s/mh%s-j%s.log source %s/run-m%s-j%s.src' % (opt.queue,opt.suffix,mass,j,logdir,mass,j,srcdir,mass,j)
+            print '   job # '+str(j)
+            if not opt.dryrun: os.system(bsub)
             
-        if not opt.queue :
-            if code: sys.exit(code)
+        if not opt.queue and not opt.dryrun:
+            if code:
+                sys.exit(code)
+                os.system(move)
 
 if __name__ == '__main__':
     main()
